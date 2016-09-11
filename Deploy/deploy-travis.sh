627dc78cb6fed26095cf14cd6ca3f05f67d570e0
seladb <pcapplusplus@gmail.com>
2016-09-12 00:50:51 +0300

    Update deploy script

--- Deploy/deploy-travis.sh
+++ Deploy/deploy-travis.sh
@@ -5,17 +5,18 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then OS_VER=Ubuntu-$(lsb_release -r | awk
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then XCODE_VER=$($(xcode-select -print-path)/usr/bin/xcodebuild -version | head -n1 | awk '{print $2}') ; fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then OS_VER=Mac-OSX-$(sw_vers -productVersion)-Xcode-$XCODE_VER; fi
 
-DIST_DIR_NAME=PcapPlusPlus-$(cat latest_release.version)-$OS_VER
+DIST_DIR_NAME=PcapPlusPlus-$(cat Deploy/latest_release.version)-$OS_VER
 
 mv Dist $DIST_DIR_NAME
 
 NEW_FIRST_LINE="PCAPPLUSPLUS_HOME := /your/PcapPlusPlus/folder"
-sed -i "1s|.*|$NEW_FIRST_LINE|" $DIST_DIR_NAME/mk/PcapPlusPlus.mk
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then SED_PARAMS="''"; fi
 
-sed -i "s|"$(PCAPPLUSPLUS_HOME)/Dist"|"$(PCAPPLUSPLUS_HOME)"|" $DIST_DIR_NAME/mk/PcapPlusPlus.mk
+sed -i $SED_PARAMS "1s|.*|$NEW_FIRST_LINE|" $DIST_DIR_NAME/mk/PcapPlusPlus.mk
+
+sed -i $SED_PARAMS "s|"$(PCAPPLUSPLUS_HOME)/Dist"|"$(PCAPPLUSPLUS_HOME)"|" $DIST_DIR_NAME/mk/PcapPlusPlus.mk
 
 cp Deploy/README.release $DIST_DIR_NAME/
-
 rm $DIST_DIR_NAME/mk/platform.mk
 
 tar -zcvf $DIST_DIR_NAME.tar.gz $DIST_DIR_NAME/
