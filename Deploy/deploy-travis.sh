f3a203c271f759b21fc14894499019e2c1910f54
seladb <elad@elad-VirtualBox.(none)>
2016-09-11 23:33:20 +0300

    Improving Travis CI deploy script

--- Deploy/deploy-travis.sh
+++ Deploy/deploy-travis.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-DIST_DIR_NAME=Dist-$TRAVIS_OS_NAME
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then OS_VER=Ubuntu-$(lsb_release -r | awk '{print $2}')-$(uname -m)-gcc-$(gcc -dumpversion); fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then XCODE_VER=$($(xcode-select -print-path)/usr/bin/xcodebuild -version | head -n1 | awk '{print $2}') ; fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then OS_VER=Mac-OSX-$(sw_vers -productVersion)-Xcode-$XCODE_VER; fi
+
+DIST_DIR_NAME=PcapPlusPlus-$(cat latest_release.version)-$OS_VER
+
 mv Dist $DIST_DIR_NAME
 
 NEW_FIRST_LINE="PCAPPLUSPLUS_HOME := /your/PcapPlusPlus/folder"
@@ -13,5 +18,5 @@ cp Deploy/README.release $DIST_DIR_NAME/
 
 rm $DIST_DIR_NAME/mk/platform.mk
 
-tar -zcvf $DIST_DIR_NAME-$TRAVIS_JOB_NUMBER.tar.gz $DIST_DIR_NAME/
+tar -zcvf $DIST_DIR_NAME.tar.gz $DIST_DIR_NAME/
 curl --upload-file ./$DIST_DIR_NAME.tar.gz https://transfer.sh/$DIST_DIR_NAME.tar.gz
