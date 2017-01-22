d1648eff7ef031e7cc14a88dcfa2f63eb5fd91f5
seladb <seladb@yahoo.com>
2017-01-22 09:08:10 +0200

    Added documentation to IgmpLayer

--- Packet++/header/IgmpLayer.h
+++ Packet++/header/IgmpLayer.h
@@ -5,66 +5,149 @@
 #include <IpAddress.h>
 #include <vector>
 
+/// @file
+
+/**
+ * \namespace pcpp
+ * \brief The main namespace for the PcapPlusPlus lib
+ */
 namespace pcpp
 {
 
+/**
+ * @struct igmp_header
+ * IGMPv1 and IGMPv2 basic protocol header
+ */
 struct igmp_header
 {
+	/** Indicates the message type. The enum for message type is pcpp::IgmpType */
 	uint8_t type;
+	/** Specifies the time limit for the corresponding report. The field has a resolution of 100 milliseconds */
 	uint8_t maxResponseTime;
+	/** This is the 16-bit one's complement of the one's complement sum of the entire IGMP message */
 	uint16_t checksum;
+	/** This is the multicast address being queried when sending a Group-Specific or Group-and-Source-Specific Query */
 	uint32_t groupAddress;
 };
 
+
+/**
+ * @struct igmpv3_query_header
+ * IGMPv3 membership query basic header
+ */
 struct igmpv3_query_header
 {
+	/** IGMP message type. Should always have value of membership query (::IgmpType_MembershipQuery)  */
 	uint8_t type;
+	/** This field specifies the maximum time (in 1/10 second) allowed before sending a responding report */
 	uint8_t maxResponseTime;
+	/** This is the 16-bit one's complement of the one's complement sum of the entire IGMP message */
 	uint16_t checksum;
+	/** This is the multicast address being queried when sending a Group-Specific or Group-and-Source-Specific Query */
 	uint32_t groupAddress;
+	/** Suppress Router-side Processing Flag + Querier's Robustness Variable */
 	uint8_t s_qrv;
+	/** Querier's Query Interval Code */
 	uint8_t qqic;
+	/** This field specifies the number of source addresses present in the Query */
 	uint16_t numOfSources;
 };
 
+
+/**
+ * @struct igmpv3_report_header
+ * IGMPv3 membership report basic header
+ */
 struct igmpv3_report_header
 {
+	/** IGMP message type. Should always have value of IGMPv3 membership report (::IgmpType_MembershipReportV3)  */
 	uint8_t type;
+	/** Unused bytes */
 	uint8_t reserved1;
+	/** This is the 16-bit one's complement of the one's complement sum of the entire IGMP message */
 	uint16_t checksum;
+	/** Unused bytes */
 	uint16_t reserved2;
+	/** This field specifies the number of group records present in the Report */
 	uint16_t numOfGroupRecords;
 };
 
+
+/**
+ * @struct igmpv3_group_record
+ * A block of fields containing information pertaining to the sender's membership in a single multicast group on the interface
+ * from which the Report is sent. Relevant only for IGMPv3 membership report messages
+ */
 struct igmpv3_group_record
 {
+	/** Group record type */
 	uint8_t recordType;
+	/** contains the length of the Auxiliary Data field in this Group Record. A value other than 0 isn't supported */
 	uint8_t auxDataLen;
+	/** The Number of Sources field specifies how many source addresses are present in this Group Record */
 	uint16_t numOfSources;
+	/** Contains the IP multicast address to which this Group Record pertains */
 	uint32_t multicastAddress;
+	/** A vector of n IP unicast addresses, where n is the value in this record's Number of Sources field */
 	uint8_t sourceAddresses[];
 
+	/**
+	 * @return The multicast address in igmpv3_group_record#multicastAddress as IPv4Address instance
+	 */
 	IPv4Address getMulticastAddress();
+
+	/**
+	 * @return The number of source addresses in this group record
+	 */
 	uint16_t getSourceAdressCount();
+
+	/**
+	 * Get the source address in a certain index
+	 * @param[in] index The index of the source address in the group record
+	 * @return The source address in the requested index. If index is negative or higher than the number of source addresses in this
+	 * group record the value if IPv4Address#Zero is returned
+	 */
 	IPv4Address getSoruceAddressAtIndex(int index);
+
+	/**
+	 * @return The total size in bytes of group record
+	 */
 	size_t getRecordLen();
 };
 
+
+/**
+ * IGMP message types
+ */
 enum IgmpType
 {
+	/** Unknown message type */
 	IgmpType_Unknown = 0,
+	/** IGMP Membership Query */
 	IgmpType_MembershipQuery = 0x11,
+	/** IGMPv1 Membership Report */
 	IgmpType_MembershipReportV1 = 0x12,
+	/** DVMRP */
 	IgmpType_DVMRP = 0x13,
+	/** PIM version 1 */
 	IgmpType_P1Mv1 = 0x14,
+	/** Cisco Trace Messages */
 	IgmpType_CiscoTrace = 0x15,
+	/** IGMPv2 Membership Report */
 	IgmpType_MembershipReportV2 = 0x16,
+	/** IGMPv2 Leave Group */
 	IgmpType_LeaveGroup = 0x17,
+	/** Multicast Traceroute Response */
 	IgmpType_MulticastTracerouteResponse = 0x1e,
+	/** Multicast Traceroute */
 	IgmpType_MulticastTraceroute = 0x1f,
+	/** IGMPv3 Membership Report */
 	IgmpType_MembershipReportV3 = 0x22,
+	/** MRD, Multicast Router Advertisement */
 	IgmpType_MulticastRouterAdvertisement = 0x30,
+	/** MRD, Multicast Router Solicitation */
 	IgmpType_MulticastRouterSolicitation = 0x31,
+	/** MRD, Multicast Router Termination */
 	IgmpType_MulticastRouterTermination = 0x32,
 };
 
