44e5e8f9af9f94570e5af038d5002f7dc87a0ce0
seladb <seladb@yahoo.com>
2017-07-20 09:51:41 +0300

    Another compilation fix in VXLAN layer

--- Packet++/src/VxlanLayer.cpp
+++ Packet++/src/VxlanLayer.cpp
@@ -1,7 +1,7 @@
 #include <VxlanLayer.h>
 #include <EthLayer.h>
 #include <string.h>
-#ifdef WIN32 //for using ntohl, ntohs, etc.
+#if defined(WIN32) || defined(WINx64) //for using ntohl, ntohs, etc.
 #include <winsock2.h>
 #elif LINUX
 #include <in.h> //for using ntohl, ntohs, etc.
