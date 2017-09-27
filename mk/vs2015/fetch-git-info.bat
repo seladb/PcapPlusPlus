51ab05eb864350cd5c8efef0be4c9d333f9d7336
seladb <seladb@yahoo.com>
2017-09-28 00:33:08 +0300

    Some fixes

--- mk/vs2015/fetch-git-info.bat
+++ mk/vs2015/fetch-git-info.bat
@@ -1,7 +1,12 @@
 set GIT_BRANCH=unavailable
 set GIT_COMMIT=unavailable
-for /f "delims=" %%a in ('git rev-parse --verify HEAD') do set GIT_BRANCH=%%a
-for /f "delims=" %%a in ('git rev-parse --abbrev-ref HEAD') do set GIT_COMMIT=%%a
+
+git rev-parse --verify HEAD
+git rev-parse --abbrev-ref HEAD
+git symbolic-ref --short HEAD
+
+for /f "delims=" %%a in ('git rev-parse --verify HEAD') do set GIT_COMMIT=%%a
+for /f "delims=" %%a in ('git symbolic-ref --short HEAD') do set GIT_BRANCH=%%a
 
 echo ^<?xml version="1.0" encoding="utf-8"?^>> GitInfoPropertySheet.props
 echo ^<Project ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003"^>>> GitInfoPropertySheet.props
