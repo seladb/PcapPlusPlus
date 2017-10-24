5d7438337efad8f4ee8f1b0dcd34ef325288a3be
seladb <seladb@yahoo.com>
2017-10-24 15:04:14 -0400

    Removed all deployment scripts (deployment moved to a separate project: PcapPlusPlus-Deploy)

--- Deploy/build-dpdk.sh
+++
@@ -1,9 +0,0 @@
-DPDK_VER=$1
-DPDK_DIR=$2
-
-sudo apt-get install linux-headers-$(uname -r)
-wget http://fast.dpdk.org/rel/dpdk-$DPDK_VER.tar.gz
-tar xvzf dpdk-$DPDK_VER.tar.gz
-cd $DPDK_DIR
-make config CC=gcc T=x86_64-native-linuxapp-gcc
-make CC=gcc
