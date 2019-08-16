50c705e027e97854cf3b425e266cf3f730eeab40
Robert Kriener <Robert.Kriener@TMEIC.com>
2019-08-16 09:50:22 -0400

    Whitespace fix

--- mk/vs2015/fetch-git-info.bat
+++ mk/vs2015/fetch-git-info.bat
@@ -12,12 +12,12 @@ echo     ^<GitCommit^>%GIT_COMMIT%^</GitCommit^>>> GitInfoPropertySheet.props
 echo     ^<GitBranch^>%GIT_BRANCH%^</GitBranch^>>> GitInfoPropertySheet.props
 echo   ^</PropertyGroup^>>> GitInfoPropertySheet.props
 echo   ^<ItemGroup^>>> GitInfoPropertySheet.props
-echo   ^<BuildMacro Include="GitCommit"^>>> GitInfoPropertySheet.props
-echo   ^<Value^>$(GitCommit)^</Value^>>> GitInfoPropertySheet.props
-echo   ^</BuildMacro^>>> GitInfoPropertySheet.props
-echo   ^<BuildMacro Include="GitBranch"^>>> GitInfoPropertySheet.props
-echo   ^<Value^>$(GitBranch)^</Value^>>> GitInfoPropertySheet.props
-echo   ^</BuildMacro^>>> GitInfoPropertySheet.props
+echo     ^<BuildMacro Include="GitCommit"^>>> GitInfoPropertySheet.props
+echo       ^<Value^>$(GitCommit)^</Value^>>> GitInfoPropertySheet.props
+echo     ^</BuildMacro^>>> GitInfoPropertySheet.props
+echo     ^<BuildMacro Include="GitBranch"^>>> GitInfoPropertySheet.props
+echo       ^<Value^>$(GitBranch)^</Value^>>> GitInfoPropertySheet.props
+echo     ^</BuildMacro^>>> GitInfoPropertySheet.props
 echo   ^</ItemGroup^>>> GitInfoPropertySheet.props
 echo   ^<PropertyGroup /^>>> GitInfoPropertySheet.props
 echo   ^<ItemDefinitionGroup /^>>> GitInfoPropertySheet.props
