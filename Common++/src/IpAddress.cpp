f01f4959d15f8a534c5ee0ba77c7e8afac40f2cf
seladb <seladb@yahoo.com>
2019-08-29 01:00:27 -0700

    Fix in IPv6Address

--- Common++/src/IpAddress.cpp
+++ Common++/src/IpAddress.cpp
@@ -170,7 +170,7 @@ IPv6Address IPv6Address::Zero(std::string("0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0"));
 
 IPv6Address::IPv6Address(const IPv6Address& other)
 {
-	m_pInAddr = new in6_addr();
+	m_pInAddr = (in6_addr*)new uint8_t[sizeof(in6_addr)];
 	memcpy(m_pInAddr, other.m_pInAddr, sizeof(in6_addr));
 
 	strncpy(m_AddressAsString, other.m_AddressAsString, MAX_ADDR_STRING_LEN-1);
@@ -190,7 +190,7 @@ IPAddress* IPv6Address::clone() const
 
 void IPv6Address::init(char* addressAsString)
 {
-	m_pInAddr = new in6_addr();
+	m_pInAddr = (in6_addr*)new uint8_t[sizeof(in6_addr)];
 	if (inet_pton(AF_INET6, addressAsString , m_pInAddr) == 0)
 	{
 		m_IsValid = false;
@@ -204,7 +204,7 @@ void IPv6Address::init(char* addressAsString)
 
 IPv6Address::IPv6Address(uint8_t* addressAsUintArr)
 {
-	m_pInAddr = new in6_addr();
+	m_pInAddr = (in6_addr*)new uint8_t[sizeof(in6_addr)];
 	memcpy(m_pInAddr, addressAsUintArr, 16);
 	if (inet_ntop(AF_INET6, m_pInAddr, m_AddressAsString, MAX_ADDR_STRING_LEN) == 0)
 		m_IsValid = false;
@@ -249,7 +249,7 @@ IPv6Address& IPv6Address::operator=(const IPv6Address& other)
 	if (m_pInAddr != NULL)
 		delete m_pInAddr;
 
-	m_pInAddr = new in6_addr();
+	m_pInAddr = (in6_addr*)new uint8_t[sizeof(in6_addr)];
 	memcpy(m_pInAddr, other.m_pInAddr, sizeof(in6_addr));
 
 	strncpy(m_AddressAsString, other.m_AddressAsString, 40);
