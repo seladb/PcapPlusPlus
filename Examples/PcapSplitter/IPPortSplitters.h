8675f1235ce3be7db67909be6d6d971806840101
gx740 <ssuslov.nn@gmail.com>
2019-12-19 10:01:09 +0300

    Fix a compilation errors in IPPortSplitter

--- Examples/PcapSplitter/IPPortSplitters.h
+++ Examples/PcapSplitter/IPPortSplitters.h
@@ -194,7 +194,7 @@ protected:
 	uint32_t getSrcIPValue(pcpp::Packet& packet)
 	{
 		if (packet.isPacketOfType(pcpp::IPv4))
-			return packet.getLayerOfType<pcpp::IPv4Layer>()->getSrcIpAddress().toInt();
+			return packet.getLayerOfType<pcpp::IPv4Layer>()->getSrcIpAddress().toUInt();
 		else if (packet.isPacketOfType(pcpp::IPv6))
 			return pcpp::fnv_hash((uint8_t*)packet.getLayerOfType<pcpp::IPv6Layer>()->getSrcIpAddress().toIn6Addr(), 16);
 		else
@@ -207,7 +207,7 @@ protected:
 	uint32_t getDstIPValue(pcpp::Packet& packet)
 	{
 		if (packet.isPacketOfType(pcpp::IPv4))
-			return packet.getLayerOfType<pcpp::IPv4Layer>()->getDstIpAddress().toInt();
+			return packet.getLayerOfType<pcpp::IPv4Layer>()->getDstIpAddress().toUInt();
 		else if (packet.isPacketOfType(pcpp::IPv6))
 			return pcpp::fnv_hash((uint8_t*)packet.getLayerOfType<pcpp::IPv6Layer>()->getDstIpAddress().toIn6Addr(), 16);
 		else
