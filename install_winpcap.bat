a23951daeeb9a3403f83ee102e6b12600254f52a
seladb <pcapplusplus@gmail.com>
2020-02-23 00:35:05 -0800

    Chanes in CI to support Npcap

---
+++ install_winpcap.bat
@@ -0,0 +1,7 @@
+git submodule update --init --recursive
+git clone https://github.com/mfontanini/winpcap-installer.git
+cd winpcap-installer
+winpcap-boundary-meter-4.1.3.exe /S
+cd ..
+appveyor DownloadFile http://www.winpcap.org/install/bin/WpdPack_4_1_2.zip
+7z x .\WpdPack_4_1_2.zip -oc:\
