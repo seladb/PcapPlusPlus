9d5cdd0b2535b0efe8216bbdce684f3fa9a90894
seladb <pcapplusplus@gmail.com>
2020-02-23 00:47:28 -0800

    Small fix in Appveyor CI job

--- ci/install_npcap.bat
+++ ci/install_npcap.bat
@@ -1,7 +1,7 @@
 appveyor DownloadFile https://nmap.org/npcap/dist/npcap-0.96.exe
 npcap-0.96.exe /S /winpcap_mode
-xcopy C:\Windows\System32\Npcap\wpcap.dll C:\Windows\System32
-xcopy C:\Windows\SysWOW64\Npcap\wpcap.dll C:\Windows\SysWOW64
+xcopy C:\Windows\System32\Npcap\*.dll C:\Windows\System32
+xcopy C:\Windows\SysWOW64\Npcap\*.dll C:\Windows\SysWOW64
 appveyor DownloadFile https://nmap.org/npcap/dist/npcap-sdk-1.04.zip
 mkdir C:\Npcap-sdk
 7z x .\npcap-sdk-1.04.zip -oC:\Npcap-sdk
