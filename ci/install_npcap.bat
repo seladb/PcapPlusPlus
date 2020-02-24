ef8fa788d6794e26ed9365bc62c174bcbbf5dbcc
seladb <pcapplusplus@gmail.com>
2020-02-24 01:04:09 -0800

    Remove Npcap from VS2019 CI job

--- ci/install_npcap.bat
+++ ci/install_npcap.bat
@@ -1,14 +1,7 @@
-appveyor DownloadFile https://nmap.org/npcap/dist/npcap-0.86.exe
-echo DONE DONWLOADING Npcap
-npcap-0.86.exe /S /winpcap_mode
-echo DONE INSTALLING
+appveyor DownloadFile https://nmap.org/npcap/dist/npcap-0.96.exe
+npcap-0.96.exe /S /winpcap_mode
 xcopy C:\Windows\System32\Npcap\*.dll C:\Windows\System32
-echo DONE COPING System32
 xcopy C:\Windows\SysWOW64\Npcap\*.dll C:\Windows\SysWOW64
-echo DONE COPING SysWOW64
 appveyor DownloadFile https://nmap.org/npcap/dist/npcap-sdk-1.04.zip
-echo DONE DONWLOADING Npcap SDK
 mkdir C:\Npcap-sdk
-echo DONE MKDIR
 7z x .\npcap-sdk-1.04.zip -oC:\Npcap-sdk
-echo DONE EXTRACTING
