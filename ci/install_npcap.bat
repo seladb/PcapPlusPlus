69dc4030b8e72c4a5db9074b3d9b0a5dc1395043
seladb <pcapplusplus@gmail.com>
2020-03-07 02:28:49 -0800

    Use Npcap OEM version with silent install in CI

--- ci/install_npcap.bat
+++ ci/install_npcap.bat
@@ -1,5 +1,6 @@
-appveyor DownloadFile https://nmap.org/npcap/dist/npcap-0.96.exe
-npcap-0.96.exe /S /winpcap_mode
+set NPCAP_OEM_FILE=npcap-0.9988-oem.exe
+curl --digest --user %NPCAP_USERNAME%:%NPCAP_PASSWORD% https://nmap.org/npcap/oem/dist/%NPCAP_OEM_FILE% --output %NPCAP_OEM_FILE%
+%NPCAP_OEM_FILE% /S /winpcap_mode
 xcopy C:\Windows\System32\Npcap\*.dll C:\Windows\System32
 xcopy C:\Windows\SysWOW64\Npcap\*.dll C:\Windows\SysWOW64
 appveyor DownloadFile https://nmap.org/npcap/dist/npcap-sdk-1.04.zip
