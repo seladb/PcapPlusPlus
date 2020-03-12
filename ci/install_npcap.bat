5c61b8f0e0d471eab606b52fdcfb4111f82eb28f
seladb <pcapplusplus@gmail.com>
2020-03-12 03:54:55 -0400

    One last change to Npcap install script

--- ci/install_npcap.bat
+++ ci/install_npcap.bat
@@ -2,14 +2,16 @@ set NPCAP_OEM_CREDENTIALS_DEFINED=0
 if defined NPCAP_USERNAME set /A NPCAP_OEM_CREDENTIALS_DEFINED=NPCAP_OEM_CREDENTIALS_DEFINED+1
 if defined NPCAP_PASSWORD set /A NPCAP_OEM_CREDENTIALS_DEFINED=NPCAP_OEM_CREDENTIALS_DEFINED+1
 
-set NPCAP_FILE=npcap
+if "%NPCAP_OEM_CREDENTIALS_DEFINED%"=="2" (
+	set NPCAP_FILE=npcap-0.9988-oem.exe
+) else (
+	set NPCAP_FILE=npcap-0.96.exe
+)
 
 if "%NPCAP_OEM_CREDENTIALS_DEFINED%"=="2" (
-    set NPCAP_FILE=npcap-0.9988-oem.exe
 	echo Using Npcap OEM version %NPCAP_FILE%
 	curl --digest --user %NPCAP_USERNAME%:%NPCAP_PASSWORD% https://nmap.org/npcap/oem/dist/%NPCAP_FILE% --output %NPCAP_FILE%
 ) else (
-	set NPCAP_FILE=npcap-0.96.exe
     echo Using Npcap free version %NPCAP_FILE%
 	appveyor DownloadFile https://nmap.org/npcap/dist/%NPCAP_FILE%
 )
