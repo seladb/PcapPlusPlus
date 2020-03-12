4a70b32269681c0cc78b663543639d14537afd9f
seladb <pcapplusplus@gmail.com>
2020-03-12 03:35:12 -0400

    CI Npcap install - fallback to free version if credentials are not provided

--- ci/install_npcap.bat
+++ ci/install_npcap.bat
@@ -1,6 +1,18 @@
-set NPCAP_OEM_FILE=npcap-0.9988-oem.exe
-curl --digest --user %NPCAP_USERNAME%:%NPCAP_PASSWORD% https://nmap.org/npcap/oem/dist/%NPCAP_OEM_FILE% --output %NPCAP_OEM_FILE%
-%NPCAP_OEM_FILE% /S /winpcap_mode
+set NPCAP_OEM_CREDENTIALS_DEFINED=0
+if defined NPCAP_USERNAME set /A NPCAP_OEM_CREDENTIALS_DEFINED=NPCAP_OEM_CREDENTIALS_DEFINED+1
+if defined NPCAP_PASSWORD set /A NPCAP_OEM_CREDENTIALS_DEFINED=NPCAP_OEM_CREDENTIALS_DEFINED+1
+
+if "%NPCAP_OEM_CREDENTIALS_DEFINED%"=="2" (
+	echo Using Npcap OEM version
+	set NPCAP_FILE=npcap-0.9988-oem.exe
+	curl --digest --user %NPCAP_USERNAME%:%NPCAP_PASSWORD% https://nmap.org/npcap/oem/dist/%NPCAP_FILE% --output %NPCAP_FILE%
+) else (
+	echo Using Npcap free version"
+	set NPCAP_FILE=npcap-0.96.exe
+	appveyor DownloadFile https://nmap.org/npcap/dist/%NPCAP_FILE%
+)
+
+%NPCAP_FILE% /S /winpcap_mode
 appveyor DownloadFile https://nmap.org/npcap/dist/npcap-sdk-1.04.zip
 mkdir C:\Npcap-sdk
 7z x .\npcap-sdk-1.04.zip -oC:\Npcap-sdk
