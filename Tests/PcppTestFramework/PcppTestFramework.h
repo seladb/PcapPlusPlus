937c1614a33b41d0cd3b04392c51dffec0107655
seladb <pcapplusplus@gmail.com>
2020-05-06 00:47:24 -0700

    Few fixes

--- Tests/PcppTestFramework/PcppTestFramework.h
+++ Tests/PcppTestFramework/PcppTestFramework.h
@@ -22,7 +22,7 @@
 #define u32_PTF_PRINT_TYPE(val) (uint32_t)(val)
 
 #ifdef PCAPPP_MINGW_ENV
-#define u64_PTF_PRINT_FORMAT "%" PRIu64
+#define u64_PTF_PRINT_FORMAT "%I64u"
 #else
 #define u64_PTF_PRINT_FORMAT "%lu"
 #endif
@@ -72,32 +72,32 @@
 	}
 
 #define PTF_ASSERT_EQUAL(actual, expected, type) \
-    if (actual != expected) { \
+	if (actual != expected) { \
 		printf("%-30s: FAILED (%s:%d). assert equal failed: actual: " type##_PTF_PRINT_FORMAT " != expected: " type##_PTF_PRINT_FORMAT "\n", __FUNCTION__, __FILE__, __LINE__, type##_PTF_PRINT_TYPE(actual), type##_PTF_PRINT_TYPE(expected)); \
 		ptfResult = 0; \
-        return; \
-    }
+		return; \
+	}
 
 #define PTF_ASSERT_NOT_EQUAL(actual, expected, type) \
-    if (actual == expected) { \
-		printf("%-30s: FAILED (%s:%d). assert not equal failed: actual: " type##_PTF_PRINT_FORMAT " != expected: " type##_PTF_PRINT_FORMAT "\n", __FUNCTION__, __FILE__, __LINE__, type##_PTF_PRINT_TYPE(actual), type##_PTF_PRINT_TYPE(expected)); \
+	if (actual == expected) { \
+		printf("%-30s: FAILED (%s:%d). assert not equal failed: actual: " type##_PTF_PRINT_FORMAT " == expected: " type##_PTF_PRINT_FORMAT "\n", __FUNCTION__, __FILE__, __LINE__, type##_PTF_PRINT_TYPE(actual), type##_PTF_PRINT_TYPE(expected)); \
 		ptfResult = 0; \
-        return; \
-    }
+		return; \
+	}
 
 #define PTF_ASSERT_GREATER_THAN(actual, expected, type) \
-    if (actual <= expected) { \
-		printf("%-30s: FAILED (%s:%d). assert greater than failed: actual: " type##_PTF_PRINT_FORMAT " != expected: " type##_PTF_PRINT_FORMAT "\n", __FUNCTION__, __FILE__, __LINE__, type##_PTF_PRINT_TYPE(actual), type##_PTF_PRINT_TYPE(expected)); \
+	if (actual <= expected) { \
+		printf("%-30s: FAILED (%s:%d). assert greater than failed: actual: " type##_PTF_PRINT_FORMAT " <= expected: " type##_PTF_PRINT_FORMAT "\n", __FUNCTION__, __FILE__, __LINE__, type##_PTF_PRINT_TYPE(actual), type##_PTF_PRINT_TYPE(expected)); \
 		ptfResult = 0; \
-        return; \
-    }
+		return; \
+	}
 
 #define PTF_ASSERT_LOWER_THAN(actual, expected, type) \
-    if (actual >= expected) { \
-		printf("%-30s: FAILED (%s:%d). assert greater than failed: actual: " type##_PTF_PRINT_FORMAT " != expected: " type##_PTF_PRINT_FORMAT "\n", __FUNCTION__, __FILE__, __LINE__, type##_PTF_PRINT_TYPE(actual), type##_PTF_PRINT_TYPE(expected)); \
+	if (actual >= expected) { \
+		printf("%-30s: FAILED (%s:%d). assert lower than failed: actual: " type##_PTF_PRINT_FORMAT " >= expected: " type##_PTF_PRINT_FORMAT "\n", __FUNCTION__, __FILE__, __LINE__, type##_PTF_PRINT_TYPE(actual), type##_PTF_PRINT_TYPE(expected)); \
 		ptfResult = 0; \
-        return; \
-    }
+		return; \
+	}
 
 #define PTF_ASSERT_BUF_COMPARE(buf1, buf2, size) \
     if (memcmp(buf1, buf2, size) != 0) { \
