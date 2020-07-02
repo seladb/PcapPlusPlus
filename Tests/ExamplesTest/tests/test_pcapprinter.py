5e2cfe4e7af3bd5b566aae1c800e11c5fbd9467f
seladb <pcapplusplus@gmail.com>
2020-07-02 02:31:04 -0700

    Convert example-tests to pytest and add IPDefragUtil tests

--- Tests/ExamplesTest/tests/test_pcapprinter.py
+++ Tests/ExamplesTest/tests/test_pcapprinter.py
@@ -1,35 +1,37 @@
 from os import path
-from .test_utils import PcppExampleTest
+import pytest
+from test_utils import run_example, text_file_contains
 
-class PcapPrinterTest(PcppExampleTest):
+class TestPcapPrinter(object):
+	pytestmark = [pytest.mark.pcapprinter, pytest.mark.no_network]
 
-	def test_sanity(self):
+	def test_sanity(self, tmpdir):
 		args = {
 			'': path.join('pcap_examples', 'many-protocols.pcap'),
-			'-o': path.join(self.temp_dir_name, 'output.txt')
+			'-o': path.join(tmpdir, 'output.txt')
 		}
-		self.run_example(example_name='PcapPrinter', args=args)
-		self.assertTextFileContain(file_path=path.join(self.temp_dir_name, 'output.txt'), expected_content='Finished. Printed 4709 packets')
+		run_example(example_name='PcapPrinter', args=args)
+		assert text_file_contains(path.join(tmpdir, 'output.txt'), expected_content='Finished. Printed 4709 packets')
 
 	def test_input_file_missing(self):
 		args = {}
-		self.run_example(example_name='PcapPrinter', args=args, expected_return_code=1)
-		self.assertStdoutContain('Error: Input file name was not given')
+		completed_process = run_example(example_name='PcapPrinter', args=args, expected_return_code=1)
+		assert 'Error: Input file name was not given' in completed_process.stdout
 
-	def test_print_count_packets(self):
+	def test_print_count_packets(self, tmpdir):
 		args = {
 			'': path.join('pcap_examples', 'many-protocols.pcap'),
-			'-o': path.join(self.temp_dir_name, 'output.txt'),
+			'-o': path.join(tmpdir, 'output.txt'),
 			'-c': '10'
 		}
-		self.run_example(example_name='PcapPrinter', args=args)
-		self.assertTextFileContain(file_path=path.join(self.temp_dir_name, 'output.txt'), expected_content='Finished. Printed 10 packets')
+		run_example(example_name='PcapPrinter', args=args)
+		assert text_file_contains(file_path=path.join(tmpdir, 'output.txt'), expected_content='Finished. Printed 10 packets')
 
-	def test_filter(self):
+	def test_filter(self, tmpdir):
 		args = {
 			'': path.join('pcap_examples', 'many-protocols.pcap'),
-			'-o': path.join(self.temp_dir_name, 'output.txt'),
+			'-o': path.join(tmpdir, 'output.txt'),
 			'-i': 'net 10.0.0.1'
 		}
-		self.run_example(example_name='PcapPrinter', args=args)
-		self.assertTextFileContain(file_path=path.join(self.temp_dir_name, 'output.txt'), expected_content='Finished. Printed 4666 packets')
+		run_example(example_name='PcapPrinter', args=args)
+		assert text_file_contains(file_path=path.join(tmpdir, 'output.txt'), expected_content='Finished. Printed 4666 packets')
--- Tests/ExamplesTest/tests/test_utils.py
+++ Tests/ExamplesTest/tests/test_utils.py
@@ -1,53 +1,28 @@
 import os
 import subprocess
 import tempfile
-from .test_file import FileTest
 
 DEFAULT_EXAMPLE_DIR = os.path.abspath('../../Dist/examples/')
 CLEANUP_TEMP_DIR = True
 
-class PcppExampleTest(FileTest):
-	def setUp(self):
-		if CLEANUP_TEMP_DIR:
-			self.__temp_dir = tempfile.TemporaryDirectory()
-			self.temp_dir_name = self.__temp_dir.name
-		else:
-			self.temp_dir_name = tempfile.mkdtemp()
-		print('created', self.temp_dir_name)
+def run_example(example_name, args, timeout=10, expected_return_code=0):
+	command_to_run = [os.path.join(DEFAULT_EXAMPLE_DIR, example_name)]
+	for flag, val in args.items():
+		if flag:
+			command_to_run.append(flag)
+		if val:
+			command_to_run.append(val)
+	print('command_to_run', command_to_run)
+	last_completed_process = subprocess.run(command_to_run, capture_output=True, text=True, timeout=timeout)
+	assert last_completed_process.returncode == expected_return_code
+	return last_completed_process
 
-	def run_example(self, example_name, args, timeout=10, expected_return_code=0):
-		command_to_run = [os.path.join(DEFAULT_EXAMPLE_DIR, example_name)]
-		for flag, val in args.items():
-			if flag:
-				command_to_run.append(flag)
-			if val:
-				command_to_run.append(val)
-		print('command_to_run', command_to_run)
-		self.last_completed_process = subprocess.run(command_to_run, capture_output=True, text=True, timeout=timeout)
-		if self.last_completed_process.returncode != expected_return_code:
-			raise AssertionError(f'Return code {self.last_completed_process.returncode} is different than expected {expected_return_code}')
+def text_file_contains(file_path, expected_content):
+	if not os.path.exists(file_path):
+		return False
 
-	def assertStdoutContain(self, expected_content):
-		if not self.last_completed_process:
-			raise AssertionError('Command was not run')
+	with open(file_path) as f:
+		return expected_content in f.read()
 
-		if not self.last_completed_process.stdout:
-			raise AssertionError('Command stdout is empty')
 
-		if not expected_content in self.last_completed_process.stdout:
-			raise AssertionError('Expected content not found in stdout')
 
-	def assertStderrContain(self, expected_content):
-		if not self.last_completed_process:
-			raise AssertionError('Command was not run')
-
-		if not self.last_completed_process.stderr:
-			raise AssertionError('Command stderr is empty')
-
-		if not expected_content in self.last_completed_process.stderr:
-			raise AssertionError('Expected content not found in stderr')
-
-	def tearDown(self):
-		if CLEANUP_TEMP_DIR:
-			print('deleting', self.temp_dir_name)
-			self.__temp_dir.cleanup()
