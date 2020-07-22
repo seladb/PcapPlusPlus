f700d36adf47e46fc32500444e2bd2b4596ade14
seladb <pcapplusplus@gmail.com>
2020-07-22 14:05:27 +0530

    Few more fixes

--- Tests/ExamplesTest/tests/test_dnsresolver.py
+++ Tests/ExamplesTest/tests/test_dnsresolver.py
@@ -5,14 +5,13 @@ from test_utils import ExampleTest
 @pytest.mark.dnsresolver
 class TestDNSResolver(ExampleTest):
 
-	# @pytest.mark.skipif(os.environ.get("SKIP_DNS_TESTS") != None, reason="Skipping tests that require DNS")
-	@pytest.mark.xfail(raises=subprocess.TimeoutExpired)
+	@pytest.mark.xfail
 	def test_sanity(self, use_sudo):
 		args = {
 			'-s': 'www.google.com',
-			'-t': '10',
+			'-t': '5',
 		}
-		completed_process = self.run_example(args=args, requires_root=use_sudo)
+		completed_process = self.run_example(args=args, timeout=5, requires_root=use_sudo)
 		assert 'IP address of [www.google.com] is:' in completed_process.stdout
 
 	@pytest.mark.no_network
@@ -29,15 +28,14 @@ class TestDNSResolver(ExampleTest):
 		completed_process = self.run_example(args=args, requires_root=use_sudo)
 		assert 'Could not resolve hostname' in completed_process.stdout
 
-	# @pytest.mark.skipif(os.environ.get("SKIP_DNS_TESTS") != None, reason="Skipping tests that require DNS")
 	@pytest.mark.interface_needed
-	@pytest.mark.xfail(raises=subprocess.TimeoutExpired)
+	@pytest.mark.xfail
 	def test_use_specific_interface(self, interface_ip_name, use_sudo):
 		assert interface_ip_name is not None
 		args = {
 			'-s': 'www.google.com',
 			'-i': interface_ip_name,
-			'-t': '10',
+			'-t': '5',
 		}
-		completed_process = self.run_example(args=args, requires_root=use_sudo)
+		completed_process = self.run_example(args=args, timeout=5, requires_root=use_sudo)
 		assert 'IP address of [www.google.com] is:' in completed_process.stdout
