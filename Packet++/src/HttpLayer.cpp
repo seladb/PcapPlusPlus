2ecb3ca778c5b0154af6fa6b8f52a9f61d68e9fb
Julian Härig <s6juhaer@uni-bonn.de>
2020-09-12 12:53:23 +0200

    Fixed missing size check when parsing HTTP layer

--- Packet++/src/HttpLayer.cpp
+++ Packet++/src/HttpLayer.cpp
@@ -114,6 +114,10 @@ std::string HttpRequestLayer::toString() const
 	static const int maxLengthToPrint = 120;
 	std::string result = "HTTP request, ";
 	int size = m_FirstLine->getSize() - 2; // the -2 is to remove \r\n at the end of the first line
+	if (size <= 0) {
+		result += std::string("CORRUPT DATA");
+		return result;
+	}
 	if (size <= maxLengthToPrint)
 	{
 		char* firstLine = new char[size+1];
