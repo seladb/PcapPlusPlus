528c3588e9d9b636424f5f0f554aed6aeb3e93bf
seladb <pcapplusplus@gmail.com>
2020-10-22 02:42:57 -0700

    Small fix

--- Examples/PcapSplitter/IPPortSplitters.h
+++ Examples/PcapSplitter/IPPortSplitters.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "Splitters.h"
+#if !defined(WIN32) && !defined(WINx64) && !defined(PCAPPP_MINGW_ENV)
+// required for including "IpUtils.h". TODO: remove it when fixing IpUtils
+#include <in.h>
+#endif
 #include "IpUtils.h"
 #include "SystemUtils.h"
 
