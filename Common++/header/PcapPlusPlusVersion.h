eac32dfac17a7bcddde6eafe2a106d819873d258
seladb <pcapplusplus@gmail.com>
2021-11-08 01:30:31 -0800

    Bump up the version to v21.11

--- Common++/header/PcapPlusPlusVersion.h
+++ Common++/header/PcapPlusPlusVersion.h
@@ -11,20 +11,20 @@
  */
 namespace pcpp
 {
-	#define PCAPPLUSPLUS_VERSION "21.05+"
-	#define PCAPPLUSPLUS_VERSION_OFFICIAL "non-official release"
+	#define PCAPPLUSPLUS_VERSION "21.11"
+	#define PCAPPLUSPLUS_VERSION_OFFICIAL "official release"
 
 	#define PCAPPLUSPLUS_VERSION_FULL "v" PCAPPLUSPLUS_VERSION " (" PCAPPLUSPLUS_VERSION_OFFICIAL ")"
 
 	/**
-	 * @return PcapPlusPlus current version, e.g: 21.05. Notice that for non-official releases (which were pulled from GitHub) the version will end with a '+'.
-	 * For example: '21.05+' means non-official release but '21.05' means official release
+	 * @return PcapPlusPlus current version, e.g: 21.11. Notice that for non-official releases (which were pulled from GitHub) the version will end with a '+'.
+	 * For example: '21.11+' means non-official release but '21.11' means official release
 	 */
 	inline std::string getPcapPlusPlusVersion() { return PCAPPLUSPLUS_VERSION; }
 
 	/**
-	 * @return PcapPlusPlus long version string which includes the version and info whether it's an official or non-official release. For example: "v21.05+ (non-official release)"
-	 * or "v21.05 (official release)"
+	 * @return PcapPlusPlus long version string which includes the version and info whether it's an official or non-official release. For example: "v21.11+ (non-official release)"
+	 * or "v21.11 (official release)"
 	 */
 	inline std::string getPcapPlusPlusVersionFull() { return PCAPPLUSPLUS_VERSION_FULL; }
 
