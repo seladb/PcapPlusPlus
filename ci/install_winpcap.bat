c1f0b4a819a7d3b7c04e368bf96b9edc529a7789
Clément Péron <peron.clem@gmail.com>
2022-02-02 14:14:47 +0100

    ci: update winpcap installer

--- ci/install_winpcap.bat
+++ ci/install_winpcap.bat
@@ -1,7 +1,10 @@
+:: Install WinPcap Runtime
 git submodule update --init --recursive
 git clone https://github.com/mfontanini/winpcap-installer.git
-cd winpcap-installer
-winpcap-boundary-meter-4.1.3.exe /S
-cd ..
-curl https://www.winpcap.org/install/bin/WpdPack_4_1_2.zip --output WpdPack_4_1_2.zip
-7z x .\WpdPack_4_1_2.zip -oc:\
+winpcap-installer\winpcap-boundary-meter-4.1.3.exe /S
+rmdir winpcap-installer /s /q
+
+:: Install WinPcap SDK
+git clone https://github.com/seladb/PcapPlusPlus-Deploy
+7z x PcapPlusPlus-Deploy\Packages\WpdPack_4_1_2.zip -oC:\
+rmdir PcapPlusPlus-Deploy /s /q
