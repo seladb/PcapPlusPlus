24cf4fa398f6a9319dd56a3b6199f8f0a99c83d2
egecetin <egecetin@hotmail.com.tr>
2022-02-09 21:21:55 +0300

    comment update + enum

--- Packet++/header/TelnetLayer.h
+++ Packet++/header/TelnetLayer.h
@@ -15,181 +15,231 @@
  */
 namespace pcpp
 {
-
-#pragma pack(push, 1)
-    struct telnet_header
-    {
-        /// "Interpret as Command" escape character
-        uint8_t interpretaion,
-        /// Command
-        command,
-        /// Option of the command
-        subcommand;
-    };
-#pragma pack(pop)
-
-    enum TelnetCommands
-    {
-        /// Marks the end of a Telnet option subnegotiation, used with the SB code to specify more specific option parameters.
-        SubnegotiationEnd = 240,
-        /// Null command; does nothing.
-        NoOperation,
-        /// Used to mark the end of a sequence of data that the recipient should scan for urgent Telnet commands.
-        DataMark,
-        /// Represents the pressing of the “break” or “attention” key on the terminal.
-        Break,
-        /// Tells the recipient to interrupt, abort, suspend or terminate the process currently in use.
-        InterruptProcess,
-        /// Instructs the remote host to continue running the current process, but discard all remaining output from it. This may be needed if a program starts to send unexpectedly large amounts of data to the user.
-        AbortOutput,
-        /// May be used to check that the remote host is still “alive”. When this character is sent the remote host returns some type of output to indicate that it is still functioning.
-        AreYouThere,
-        /// Instructs the recipient to delete the last undeleted character from the data stream. Used to “undo” the sending of a character.
-        EraseCharacter,
-        /// Tells the recipient to delete all characters from the data stream back to (but not including) the last end of line (CR+LF) sequence.
-        EraseLine,
-        /// Used in Telnet half-duplex mode to signal the other device that it may transmit.
-        GoAhead,
-        /// Marks the beginning of a Telnet option subnegotiation, used when an option requires the client and server to exchange parameters.
-        Subnegotiation,
-        /// Indicates that the device sending this code is willing to perform or continue performing a particular option.
-        WillPerform,
-        /// Indicates that the device sending this code is either not willing to perform a particular option, or is now refusing to continue to perform it.
-        WontPerform,
-        /// Requests that the other device perform a particular option or confirms the expectation that the other device will perform that option.
-        DoPerform,
-        /// Specifies that the other party not perform an option, or confirms a device’s expectation that the other party not perform an option.
-        DontPerform,
-        /// Precedes command values 240 through 254 as described above. A pair of IAC bytes in a row represents the data value 255.
-        InterpretAsCommand
-    };
-
-    enum TelnetOptions
-    {
-        /// RFC856 https://www.iana.org/go/rfc856
-        BinaryTransmission = 0,
-        /// RFC857 https://www.iana.org/go/rfc857
-        Echo,
-        Reconnection,
-        /// RFC858 https://www.iana.org/go/rfc858
-        SuppressGoAhead,
-        ApproxMsgSizeNegotiation,
-        /// RFC859 https://www.iana.org/go/rfc859
-        Status,
-        /// RFC860 https://www.iana.org/go/rfc860
-        TimingMark,
-        /// RFC726 https://www.iana.org/go/rfc726
-        RemoteControlledTransAndEcho,
-        OutputLineWidth,
-        OutputPageSize,
-        /// RFC652 https://www.iana.org/go/rfc652
-        OutputCarriageReturnDisposition,
-        /// RFC653 https://www.iana.org/go/rfc653
-        OutputHorizontalTabStops,
-        /// RFC654 https://www.iana.org/go/rfc654
-        OutputHorizontalTabDisposition,
-        /// RFC655 https://www.iana.org/go/rfc655
-        OutputFormfeedDisposition,
-        /// RFC656 https://www.iana.org/go/rfc656
-        OutputVerticalTabStops,
-        /// RFC657 https://www.iana.org/go/rfc657
-        OutputVerticalTabDisposition,
-        /// RFC658 https://www.iana.org/go/rfc658
-        OutputLinefeedDisposition,
-        /// RFC698 https://www.iana.org/go/rfc698
-        ExtendedASCII,
-        /// RFC727 https://www.iana.org/go/rfc727
-        Logout,
-        /// RFC735 https://www.iana.org/go/rfc735
-        ByteMacro,
-        /// RFC1043 - RFC732 https://www.iana.org/go/rfc1043 https://www.iana.org/go/rfc732
-        DataEntryTerminal,
-        /// RFC736 - RFC734 https://www.iana.org/go/rfc736 https://www.iana.org/go/rfc734
-        SUPDUP,
-        /// RFC749 https://www.iana.org/go/rfc749
-        SUPDUPOutput,
-        /// RFC779 https://www.iana.org/go/rfc779
-        SendLocation,
-        /// RFC1091 https://www.iana.org/go/rfc1091
-        TerminalType,
-        /// RFC885 https://www.iana.org/go/rfc885
-        EndOfRecord,
-        /// RFC927 https://www.iana.org/go/rfc927
-        TACACSUserIdentification,
-        /// RFC933 https://www.iana.org/go/rfc933
-        OutputMarking,
-        /// RFC946 https://www.iana.org/go/rfc946
-        TerminalLocationNumber,
-        /// RFC1041 https://www.iana.org/go/rfc1041
-        Telnet3270Regime,
-        /// RFC1053 https://www.iana.org/go/rfc1053
-        X3Pad,
-        /// RFC1073 https://www.iana.org/go/rfc1073
-        NegotiateAboutWindowSize,
-        /// RFC1079 https://www.iana.org/go/rfc1079
-        TerminalSpeed,
-        /// RFC1372 https://www.iana.org/go/rfc1372
-        RemoteFlowControl,
-        /// RFC1184 https://www.iana.org/go/rfc1184
-        Linemode,
-        /// RFC1096 https://www.iana.org/go/rfc1096
-        XDisplayLocation,
-        /// RFC1408 https://www.iana.org/go/rfc1408
-        EnvironmentOption,
-        /// RFC2941 https://www.iana.org/go/rfc2941
-        AuthenticationOption,
-        /// RFC2946 https://www.iana.org/go/rfc2946
-        EncryptionOption,
-        /// RFC1572 https://www.iana.org/go/rfc1572
-        NewEnvironmentOption,
-        /// RFC2355 https://www.iana.org/go/rfc2355
-        TN3270E,
-        XAuth,
-        /// RFC2066 https://www.iana.org/go/rfc2066
-        Charset,
-        TelnetRemoteSerialPort,
-        /// RFC2217 https://www.iana.org/go/rfc2217
-        ComPortControlOption,
-        TelnetSuppressLocalEcho,
-        TelnetStartTLS,
-        /// RFC2840 https://www.iana.org/go/rfc2840
-        Kermit,
-        SendURL,
-        ForwardX,
-
-        TelOptPragmaLogon = 138,
-        TelOptSSPILogon,
-        TelOptPragmaHeartbeat,
-
-        ExtendedOptions = 255
-
-    };
-
+    /**
+     * Class for representing the Telnet Layer
+     */
     class TelnetLayer : public Layer
     {
     private:
+#pragma pack(push, 1)
+        struct telnet_header
+        {
+            /// "Interpret as Command" escape character
+            uint8_t interpretaion,
+            /// Command
+            command,
+            /// Option of the command
+            subcommand;
+        };
+#pragma pack(pop)
+
         bool isData;
         telnet_header *getTelnetHeader() const { return (telnet_header *)m_Data; }
 
     public:
+    
         /**
-		 * A static method that checks whether the port is considered as Telnet
-		 * @param[in] port The port number to be checked
-		 */
+         * Telnet Command Indicator
+         */
+        enum TelnetCommands
+        {
+            /// Marks the end of a Telnet option subnegotiation, used with the SB code to specify more specific option parameters.
+            SubnegotiationEnd = 240,
+            /// Null command; does nothing.
+            NoOperation,
+            /// Used to mark the end of a sequence of data that the recipient should scan for urgent Telnet commands.
+            DataMark,
+            /// Represents the pressing of the “break” or “attention” key on the terminal.
+            Break,
+            /// Tells the recipient to interrupt, abort, suspend or terminate the process currently in use.
+            InterruptProcess,
+            /// Instructs the remote host to continue running the current process, but discard all remaining output from it. This may be needed if a program starts to send unexpectedly large amounts of data to the user.
+            AbortOutput,
+            /// May be used to check that the remote host is still “alive”. When this character is sent the remote host returns some type of output to indicate that it is still functioning.
+            AreYouThere,
+            /// Instructs the recipient to delete the last undeleted character from the data stream. Used to “undo” the sending of a character.
+            EraseCharacter,
+            /// Tells the recipient to delete all characters from the data stream back to (but not including) the last end of line (CR+LF) sequence.
+            EraseLine,
+            /// Used in Telnet half-duplex mode to signal the other device that it may transmit.
+            GoAhead,
+            /// Marks the beginning of a Telnet option subnegotiation, used when an option requires the client and server to exchange parameters.
+            Subnegotiation,
+            /// Indicates that the device sending this code is willing to perform or continue performing a particular option.
+            WillPerform,
+            /// Indicates that the device sending this code is either not willing to perform a particular option, or is now refusing to continue to perform it.
+            WontPerform,
+            /// Requests that the other device perform a particular option or confirms the expectation that the other device will perform that option.
+            DoPerform,
+            /// Specifies that the other party not perform an option, or confirms a device’s expectation that the other party not perform an option.
+            DontPerform,
+            /// Precedes command values 240 through 254 as described above. A pair of IAC bytes in a row represents the data value 255.
+            InterpretAsCommand
+        };
+
+        /**
+         * Telnet Options
+         */
+        enum TelnetOptions
+        {
+            /// Binary Transmission RFC856 https://www.iana.org/go/rfc856
+            TransmitBinary = 0,
+            /// Echo RFC857 https://www.iana.org/go/rfc857
+            Echo,
+            /// Reconnection
+            Reconnection,
+            /// Suppress Go Ahead RFC858 https://www.iana.org/go/rfc858
+            SuppressGoAhead,
+            /// Negotiate approximate message size
+            ApproxMsgSizeNegotiation,
+            /// Status RFC859 https://www.iana.org/go/rfc859
+            Status,
+            /// Timing Mark RFC860 https://www.iana.org/go/rfc860
+            TimingMark,
+            /// RCTE, Remote Controlled Transmission and Echo RFC726 https://www.iana.org/go/rfc726
+            RemoteControlledTransAndEcho,
+            /// Output Line Width
+            OutputLineWidth,
+            /// Output Page Size
+            OutputPageSize,
+            /// NAOCRD, Negotiate About Output Carriage-Return Disposition RFC652 https://www.iana.org/go/rfc652
+            OutputCarriageReturnDisposition,
+            /// NAOHTS, Negotiate About Output Horizontal Tabstops RFC653 https://www.iana.org/go/rfc653
+            OutputHorizontalTabStops,
+            /// NAOHTD, Negotiate About Output Horizontal Tab Disposition RFC654 https://www.iana.org/go/rfc654
+            OutputHorizontalTabDisposition,
+            /// NAOFFD, Negotiate About Output Formfeed Disposition RFC655 https://www.iana.org/go/rfc655
+            OutputFormfeedDisposition,
+            /// NAOVTS, Negotiate About Vertical Tabstops RFC656 https://www.iana.org/go/rfc656
+            OutputVerticalTabStops,
+            /// NAOVTD, Negotiate About Output Vertcial Tab Disposition RFC657 https://www.iana.org/go/rfc657
+            OutputVerticalTabDisposition,
+            /// NAOLFD, Negotiate About Output Linefeed Disposition RFC658 https://www.iana.org/go/rfc658
+            OutputLinefeedDisposition,
+            /// Extended ASCII RFC698 https://www.iana.org/go/rfc698
+            ExtendedASCII,
+            /// Logout RFC727 https://www.iana.org/go/rfc727
+            Logout,
+            /// BM, Byte Macro RFC735 https://www.iana.org/go/rfc735
+            ByteMacro,
+            /// Data Entry Terminal RFC1043 - RFC732 https://www.iana.org/go/rfc1043 https://www.iana.org/go/rfc732
+            DataEntryTerminal,
+            /// SUPDUP RFC736 - RFC734 https://www.iana.org/go/rfc736 https://www.iana.org/go/rfc734
+            SUPDUP,
+            /// SUPDUP Output RFC749 https://www.iana.org/go/rfc749
+            SUPDUPOutput,
+            /// Send Location RFC779 https://www.iana.org/go/rfc779
+            SendLocation,
+            /// Terminal Type RFC1091 https://www.iana.org/go/rfc1091
+            TerminalType,
+            /// End of record RFC885 https://www.iana.org/go/rfc885
+            EndOfRecord,
+            /// TUID, TACACS User Identification RFC927 https://www.iana.org/go/rfc927
+            TACACSUserIdentification,
+            /// OUTMRK, Output Marking RFC933 https://www.iana.org/go/rfc933
+            OutputMarking,
+            /// TTYLOC, Terminal Location Number RFC946 https://www.iana.org/go/rfc946
+            TerminalLocationNumber,
+            /// Telnet 3270 Regime RFC1041 https://www.iana.org/go/rfc1041
+            Telnet3270Regime,
+            /// X.3 PAD RFC1053 https://www.iana.org/go/rfc1053
+            X3Pad,
+            /// NAWS, Negotiate About Window Size RFC1073 https://www.iana.org/go/rfc1073
+            NegotiateAboutWindowSize,
+            /// Terminal Speed RFC1079 https://www.iana.org/go/rfc1079
+            TerminalSpeed,
+            /// Remote Flow Control RFC1372 https://www.iana.org/go/rfc1372
+            RemoteFlowControl,
+            /// Line Mode RFC1184 https://www.iana.org/go/rfc1184
+            Linemode,
+            /// X Display Location RFC1096 https://www.iana.org/go/rfc1096
+            XDisplayLocation,
+            /// Environment Option RFC1408 https://www.iana.org/go/rfc1408
+            EnvironmentOption,
+            /// Authentication Option RFC2941 https://www.iana.org/go/rfc2941
+            AuthenticationOption,
+            /// Encryption Option RFC2946 https://www.iana.org/go/rfc2946
+            EncryptionOption,
+            /// New Environment Option RFC1572 https://www.iana.org/go/rfc1572
+            NewEnvironmentOption,
+            /// TN3270E RFC2355 https://www.iana.org/go/rfc2355
+            TN3270E,
+            /// X Server Authentication
+            XAuth,
+            /// Charset RFC2066 https://www.iana.org/go/rfc2066
+            Charset,
+            /// RSP, Telnet Remote Serial Port
+            TelnetRemoteSerialPort,
+            /// Com Port Control Option RFC2217 https://www.iana.org/go/rfc2217
+            ComPortControlOption,
+            /// Telnet Suppress Local Echo
+            TelnetSuppressLocalEcho,
+            /// Telnet Start TLS
+            TelnetStartTLS,
+            /// Kermit RFC2840 https://www.iana.org/go/rfc2840
+            Kermit,
+            /// Send URL
+            SendURL,
+            /// Forward X Server
+            ForwardX,
+
+            /// Telnet Option Pragma Logon
+            TelOptPragmaLogon = 138,
+            /// Telnet Option SSPI Logon
+            TelOptSSPILogon,
+            /// Telnet Option Pragma Heartbeat
+            TelOptPragmaHeartbeat,
+
+            /// Extended option list
+            ExtendedOptions = 255
+
+        };
+
+
+
+        /**
+         * Convert the Telnet Command to readable string
+         * @param[in] val Value of the command
+         * @return The Telnet Command as readable string
+         */
+        static std::string getTelnetCommandAsString(TelnetCommands val);
+
+        /**
+         * Convert the Telnet option to readable string
+         * @param[in] val Value of the option
+         * @return The Telnet Option as readable string
+         */
+        static std::string getTelnetOptionAsString(TelnetOptions val);
+
+        /**
+         * A static method that checks whether the port is considered as Telnet
+         * @param[in] port The port number to be checked
+         */
         static bool isTelnetPort(uint16_t port) { return port == 23; }
 
         // overridden methods
 
+        /// Parses the next layer. Telnet is the always last so does nothing for this layer
         void parseNextLayer() {}
 
+        /**
+         * @return Get the size of the layer
+         */
         size_t getHeaderLen() const { return m_DataLen; }
 
+        /// Does nothing for this layer
         void computeCalculateFields() {}
 
+        /**
+         * @return The OSI layer level of Telnet (Application Layer).
+         */
         OsiModelLayer getOsiModelLayer() const { return OsiModelApplicationLayer; }
 
+        /**
+         * @return Returns the protocol info as readable string
+         */
         std::string toString() const;
-    }
+    };
 
 } // namespace pcpp
 
--- Packet++/src/TelnetLayer.cpp
+++ Packet++/src/TelnetLayer.cpp
@@ -5,6 +5,163 @@
 
 namespace pcpp
 {
+    std::string TelnetLayer::getTelnetCommandAsString(TelnetCommands val)
+    {
+        switch(val)
+        {
+            case SubnegotiationEnd:
+                return "Subnegotiation End";
+            case NoOperation:
+                return "No Operation";
+            case DataMark:
+                return "Data Mark";
+            case Break:
+                return "Break";
+            case InterruptProcess:
+                return "Interrupt Process";
+            case AbortOutput:
+                return "Abort Output";
+            case AreYouThere:
+                return "Are You There";
+            case EraseCharacter:
+                return "Erase Character";
+            case EraseLine:
+                return "Erase Line";
+            case GoAhead:
+                return "Go Ahead";
+            case Subnegotiation:
+                return "Subnegotiation";
+            case WillPerform:
+                return "Will Perform";
+            case WontPerform:
+                return "Wont Perform";
+            case DoPerform:
+                return "Do Perform";
+            case DontPerform:
+                return "Dont Perform";
+            case InterpretAsCommand:
+                return "Interpret As Command";
+            default:
+                return "Unknown Command";
+        }
+    }
+
+    std::string TelnetLayer::getTelnetOptionAsString(TelnetOptions val)
+    {
+        switch (val)
+        {
+            case TransmitBinary:
+                return "Binary Transmission";
+            case Echo:
+                return "Echo";
+            case Reconnection:
+                return "Reconnection";
+            case SuppressGoAhead:
+                return "Suppress Go Ahead";
+            case ApproxMsgSizeNegotiation:
+                return "Negotiate approximate message size";
+            case Status:
+                return "Status";
+            case TimingMark:
+                return "Timing Mark";
+            case RemoteControlledTransAndEcho:
+                return "Remote Controlled Transmission and Echo";
+            case OutputLineWidth:
+                return "Output Line Width";
+            case OutputPageSize:
+                return "Output Page Size";
+            case OutputCarriageReturnDisposition:
+                return "Negotiate About Output Carriage-Return Disposition";
+            case OutputHorizontalTabStops:
+                return "Negotiate About Output Horizontal Tabstops";
+            case OutputHorizontalTabDisposition:
+                return "Negotiate About Output Horizontal Tab Disposition";
+            case OutputFormfeedDisposition:
+                return "Negotiate About Output Formfeed Disposition";
+            case OutputVerticalTabStops:
+                return "Negotiate About Vertical Tabstops";
+            case OutputVerticalTabDisposition:
+                return "Negotiate About Output Vertcial Tab Disposition";
+            case OutputLinefeedDisposition:
+                return "Negotiate About Output Linefeed Disposition";
+            case ExtendedASCII:
+                return "Extended ASCII";
+            case Logout:
+                return "Logout";
+            case ByteMacro:
+                return "Byte Macro";
+            case DataEntryTerminal:
+                return "Data Entry Terminal";
+            case SUPDUP:
+                return "SUPDUP";
+            case SUPDUPOutput:
+                return "SUPDUP Output";
+            case SendLocation:
+                return "Send Location";
+            case TerminalType:
+                return "Terminal Type";
+            case EndOfRecord:
+                return "End Of Record";
+            case TACACSUserIdentification:
+                return "TACACS User Identification";
+            case OutputMarking:
+                return "Output Marking";
+            case TerminalLocationNumber:
+                return "Terminal Location Number";
+            case Telnet3270Regime:
+                return "Telnet 3270 Regime";
+            case X3Pad:
+                return "X3 Pad";
+            case NegotiateAboutWindowSize:
+                return "Negotiate About Window Size";
+            case TerminalSpeed:
+                return "Terminal Speed";
+            case RemoteFlowControl:
+                return "Remote Flow Control";
+            case Linemode:
+                return "Line mode";
+            case XDisplayLocation:
+                return "X Display Location";
+            case EnvironmentOption:
+                return "Environment Option";
+            case AuthenticationOption:
+                return "Authentication Option";
+            case EncryptionOption:
+                return "Encryption Option";
+            case NewEnvironmentOption:
+                return "New Environment Option";
+            case TN3270E:
+                return "TN3270E";
+            case XAuth:
+                return "X Server Authentication";
+            case Charset:
+                return "Charset";
+            case TelnetRemoteSerialPort:
+                return "Telnet Remote Serial Port";
+            case ComPortControlOption:
+                return "Com Port Control Option";
+            case TelnetSuppressLocalEcho:
+                return "Telnet Suppress Local Echo";
+            case TelnetStartTLS:
+                return "Telnet Start TLS";
+            case Kermit:
+                return "Kermit";
+            case SendURL:
+                return "Send URL";
+            case ForwardX:
+                return "Forward X Server";
+            case TelOptPragmaLogon:
+                return "Telnet Option Pragma Logon";
+            case TelOptSSPILogon:
+                return "Telnet Option SSPI Logon";
+            case TelOptPragmaHeartbeat:
+                return "Telnet Option Pragma Heartbeat";
+            case ExtendedOptions:
+                return "Extended option list";
+            default:
+                return "Unknown Option";
+        }
+    }
 
     std::string TelnetLayer::toString() const
     {
