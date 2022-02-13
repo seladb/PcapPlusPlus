54e079635f6565bc79dc99d69b63582b9ae95d05
egecetin <egecetin@hotmail.com.tr>
2022-02-13 21:55:04 +0300

    fix

--- Packet++/src/IPv4Layer.cpp
+++ Packet++/src/IPv4Layer.cpp
@@ -321,6 +321,9 @@ void IPv4Layer::parseNextLayer()
 			? static_cast<Layer*>(new ESPLayer(payload, payloadLen, this, m_Packet))
 			: static_cast<Layer*>(new PayloadLayer(payload, payloadLen, this, m_Packet));
 		break;
+	case PACKETPP_IPPROTO_IPV6:
+		m_NextLayer = new IPv6Layer(payload, payloadLen, this, m_Packet);
+		break;
 	default:
 		m_NextLayer = new PayloadLayer(payload, payloadLen, this, m_Packet);
 	}
