2164a2b345a41f78c6fef25ef9774706fb9c92a8
seladb <pcapplusplus@gmail.com>
2022-02-18 02:13:21 -0800

    Black formatter fixes

--- ci/patch_mingw.py
+++ ci/patch_mingw.py
@@ -4,14 +4,16 @@ import fileinput
 MINGW_PATH = "C:\\MinGW"
 ptw32_errno_h_path = path.join(MINGW_PATH, "include", "ptw32_errno.h")
 
+
 def main():
-	with fileinput.FileInput(ptw32_errno_h_path, inplace=True, backup='.bak') as f:
-		for line in f:
-			if line.startswith("# include <winsock.h>"):
-				print("//", line, end='')
-			else:
-				print(line, end='')
-	print("Done patching MinGW")
+    with fileinput.FileInput(ptw32_errno_h_path, inplace=True, backup=".bak") as f:
+        for line in f:
+            if line.startswith("# include <winsock.h>"):
+                print("//", line, end="")
+            else:
+                print(line, end="")
+    print("Done patching MinGW")
+
 
 if __name__ == "__main__":
-	main()
+    main()
