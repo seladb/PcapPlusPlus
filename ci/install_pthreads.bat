b3890ebaf2fa7a8571b11f51aa2868d6b969ac99
seladb <pcapplusplus@gmail.com>
2022-03-22 00:54:48 -0700

    Fix

--- ci/install_pthreads.bat
+++ ci/install_pthreads.bat
@@ -1,3 +1,3 @@
-clone https://github.com/seladb/PcapPlusPlus-Deploy
+git clone https://github.com/seladb/PcapPlusPlus-Deploy
 7z x PcapPlusPlus-Deploy\Packages\pthreads-w32-2-9-1-release.zip -oC:\pthreads
 rmdir PcapPlusPlus-Deploy /s /q
