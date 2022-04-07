4fe59f0e8e56a7ff9ab07abf902d32613b7b9343
seladb <pcapplusplus@gmail.com>
2022-04-06 23:15:40 -0700

    Remove deprecated methods in `IPv4Layer` and `IPv6Layer`

--- Packet++/header/IPv4Layer.h
+++ Packet++/header/IPv4Layer.h
@@ -8,17 +8,6 @@
 #include <string.h>
 #include <vector>
 
-#ifndef PCPP_DEPRECATED
-#if defined(__GNUC__) || defined(__clang__)
-#define PCPP_DEPRECATED __attribute__((deprecated))
-#elif defined(_MSC_VER)
-#define PCPP_DEPRECATED __declspec(deprecated)
-#else
-#pragma message("WARNING: DEPRECATED feature is not implemented for this compiler")
-#define PCPP_DEPRECATED
-#endif
-#endif
-
 /// @file
 
 /**
@@ -477,11 +466,6 @@ namespace pcpp
 		IPAddress getSrcIPAddress() const { return getSrcIPv4Address(); }
 
 		/**
-		 * @deprecated Please use getSrcIPv4Address() or getSrcIPAddress() instead
-		 */
-		PCPP_DEPRECATED IPv4Address getSrcIpAddress() const { return getSrcIPv4Address(); }
-
-		/**
 		 * Get the source IP address in the form of IPv4Address
 		 * @return An IPv4Address containing the source address
 		 */
@@ -499,11 +483,6 @@ namespace pcpp
 		 * @return An IPAddress containing the destination address
 		 */
 		IPAddress getDstIPAddress() const { return getDstIPv4Address(); }
-
-		/**
-		 * @deprecated Please use getDstIPv4Address() or getDstIPAddress() instead
-		 */
-		PCPP_DEPRECATED IPv4Address getDstIpAddress() const { return getDstIPv4Address(); }
 
 		/**
 		 * Get the destination IP address in the form of IPv4Address
