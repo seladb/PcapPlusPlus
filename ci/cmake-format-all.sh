25abc9f2f0d39360f38ea2873373a18f1a01b60a
Clément Péron <peron.clem@gmail.com>
2022-09-03 17:35:58 +0200

    ci: don't indent 3rdParty cmake files

--- ci/cmake-format-all.sh
+++ ci/cmake-format-all.sh
@@ -5,4 +5,4 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "${SCRIPT}")
 ROOTPATH=${SCRIPTPATH}/..
 
-find "${ROOTPATH}" \( -name '*.cmake' -o -name 'CMakeLists.txt' \) -exec echo 'Formatting:' {} ';' -exec cmake-format -i {} ';'
+find "${ROOTPATH}" \( -name '*.cmake' -o -name 'CMakeLists.txt' \) -not -path "*/3rdParty/*" -exec echo 'Formatting:' {} ';' -exec cmake-format -i {} ';'
