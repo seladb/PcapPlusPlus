500eceb968a965b4c0c5d961b4adc75123229c12
Ege Çetin <egecetin@hotmail.com.tr>
2022-10-14 20:53:29 +0000

    update python script

--- 3rdParty/OUILookup/createOUIData.py
+++ 3rdParty/OUILookup/createOUIData.py
@@ -11,55 +11,81 @@ countSuccess = 0
 countFail = 0
 readMAC = ''
 readVendor = ''
-oldMacAddress = 0
 mainJson = {}
-bufferJson = {}
+vMask = []
+vMaskedVendors = {}
 
 # Every line is a new MAC address
 for line in Lines:
     try:
+        readMask = 0
+        readMACShort = ''
+        readMACLong = ''
+        readVendor = ''
+
         # Prepare line
         line = line.replace('"', "")
         splitLine = line.split("\t")
 
-        if len(splitLine) >= 3 and len(splitLine[0]) == 8:
-            readMAC = splitLine[0].lower().strip()
-            readVendor = splitLine[2].strip()
-        elif len(splitLine) == 2 and len(splitLine[0]) == 8:
-            readMAC = splitLine[0].lower().strip()
+        # MAC + Short name
+        if len(splitLine) == 2 and len(splitLine[0]) == 8:
+            readMACShort = splitLine[0].lower().strip()
             readVendor = splitLine[1].strip()
+        # MAC + Short name + Long name
+        elif len(splitLine) >= 3 and len(splitLine[0]) == 8:
+            readMACShort = splitLine[0].lower().strip()
+            readVendor = splitLine[2].strip()
+        # MAC/Mask + Short name
         elif len(splitLine) == 2 and len(splitLine[0]) > 8 and len(splitLine[0]) < 21:
             maskSplit = splitLine[0].split("/")
             if len(maskSplit) == 2:
+                readMACLong = maskSplit[0].lower().strip()
+                readMACShort = maskSplit[0][0:8].lower().strip()
+                readVendor = splitLine[1].strip()
                 readMask = int(maskSplit[1])
             else:
                 raise Exception("Unknown number of elements for masking long MAC address", line)
+        # MAC/Mask + Short name + Long name
         elif len(splitLine) == 3 and len(splitLine[0]) > 8 and len(splitLine[0]) < 21:
             maskSplit = splitLine[0].split("/")
             if len(maskSplit) == 2:
+                readMACLong = maskSplit[0].lower().strip()
+                readMACShort = maskSplit[0][0:8].lower().strip()
+                readVendor = splitLine[1].strip()
                 readMask = int(maskSplit[1])
             else:
                 raise Exception("Unknown number of elements for masking long MAC address", line)
+        # Comment lines
         elif (line[0] != '#') or (line[0] != '\n'):
-            raise Exception("") 
+            raise Exception("")
         else:
             raise Exception("Unkown number of elements for line", line)
         
-        if len(readMAC) == 8: # If equal to 8 should be a non-masked (short) MAC address
-            currentMacAddress = int(readMAC.replace(":",''), 16)
-            mainJson.update({currentMacAddress:{"vendor":readVendor}})
-            oldMacAddress == currentMacAddress
-        else: # Otherwise this should be a masked (long) MAC address
-            continue # <--------------------------
+        # If equal to 0 should be a non-masked (short) MAC address
+        if readMask == 0:
+            if len(vMask):
+                mainJson[currentMacAddressShort]["maskedFilters"] = []
+                for i in range(0, len(vMask)):
+                    mainJson[currentMacAddressShort]["maskedFilters"].append({"mask":vMask[i], "vendors":vMaskedVendors[i]})
+            currentMacAddressShort = int(readMACShort.replace(":",''), 16)
+            mainJson[currentMacAddressShort] = {"vendor":readVendor}
+            vMask = []
+            vMaskedVendors = []
+        # Otherwise this should be a masked (long) MAC address
+        else:
+            currentMacAddressLong = int(readMACLong.replace(":",''), 16)
+            if readMask in vMask:
+                indx = vMask.index(readMask)
+            else:
+                vMask.append(readMask)
+                vMaskedVendors.append({})
+                indx = len(vMask) - 1
+            vMaskedVendors[indx][currentMacAddressLong] = readVendor
         countSuccess = countSuccess + 1
     except Exception as e:
         if hasattr(e, 'message'):
             print(e)
             countFail = countFail + 1
-        readMAC = ''
-        readVendor = ''
-        readMask = 0
-        continue
 
 print("Total number of vendors is", countSuccess, "failed", countFail)
 print("Writing file")
