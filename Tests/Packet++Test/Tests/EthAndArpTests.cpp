dbcba202e53223a1d1ba994e6651d38de478bfe5
Ege Çetin <egecetin@hotmail.com.tr>
2022-10-18 15:19:51 +0000

    change test oui's with less advertising brands

--- Tests/Packet++Test/Tests/EthAndArpTests.cpp
+++ Tests/Packet++Test/Tests/EthAndArpTests.cpp
@@ -18,15 +18,15 @@ PTF_TEST_CASE(OUILookup)
 	PTF_ASSERT_EQUAL(lookupEngine.getVendorName(pcpp::MacAddress("aa:aa:aa:aa:aa:aa")), "Unknown");
 
 	// CIDR 36
-	PTF_ASSERT_EQUAL(lookupEngine.getVendorName(pcpp::MacAddress("70:B3:D5:34:A0:00")), "Pavo Tasarim Üreti̇m Ti̇c A.Ş.");
-	PTF_ASSERT_EQUAL(lookupEngine.getVendorName(pcpp::MacAddress("70:B3:D5:34:AF:FF")), "Pavo Tasarim Üreti̇m Ti̇c A.Ş.");
+	PTF_ASSERT_EQUAL(lookupEngine.getVendorName(pcpp::MacAddress("70:B3:D5:2A:B0:00")), "NASA Johnson Space Center");
+	PTF_ASSERT_EQUAL(lookupEngine.getVendorName(pcpp::MacAddress("70:B3:D5:2A:BF:FF")), "NASA Johnson Space Center");
 
 	// CIDR 28
 	PTF_ASSERT_EQUAL(lookupEngine.getVendorName(pcpp::MacAddress("f4:0e:11:f0:00:00")), "Private");
 	PTF_ASSERT_EQUAL(lookupEngine.getVendorName(pcpp::MacAddress("f4:0e:11:ff:ff:ff")), "Private");
 
 	// Short
-	PTF_ASSERT_EQUAL(lookupEngine.getVendorName(pcpp::MacAddress("00:0A:35:01:01:01")), "Xilinx");
+	PTF_ASSERT_EQUAL(lookupEngine.getVendorName(pcpp::MacAddress("00:08:55:01:01:01")), "NASA-Goddard Space Flight Center");
 }
 
 PTF_TEST_CASE(EthPacketCreation)
