8a3463e9b7039d4469c66d81fd5312d83367332b
Szókovács Róbert <szo@szo.hu>
2022-10-20 15:11:56 +0200

    make the programname provided to dpdk configurable, otherwise it will overwrite the application's syslog ident

--- Pcap++/src/DpdkDeviceList.cpp
+++ Pcap++/src/DpdkDeviceList.cpp
@@ -70,7 +70,7 @@ DpdkDeviceList::~DpdkDeviceList()
 	m_DpdkDeviceList.clear();
 }
 
-bool DpdkDeviceList::initDpdk(CoreMask coreMask, uint32_t mBufPoolSizePerDevice, uint8_t masterCore, uint32_t initDpdkArgc, char **initDpdkArgv)
+bool DpdkDeviceList::initDpdk(CoreMask coreMask, uint32_t mBufPoolSizePerDevice, uint8_t masterCore, uint32_t initDpdkArgc, char **initDpdkArgv, const char *prgname)
 {
 	char **initDpdkArgvBuffer;
 
@@ -100,7 +100,7 @@ bool DpdkDeviceList::initDpdk(CoreMask coreMask, uint32_t mBufPoolSizePerDevice,
 
 
 	std::stringstream dpdkParamsStream;
-	dpdkParamsStream << "pcapplusplusapp ";
+	dpdkParamsStream << prgname << " ";
 	dpdkParamsStream << "-n ";
 	dpdkParamsStream << "2 ";
 	dpdkParamsStream << "-c ";
