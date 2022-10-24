c5d93403422eae4c6ecaa8ac6a2c728fba9f2b9c
Jafar Gholamzadeh <jafar.gholamzadeh@digikala.com>
2022-10-24 20:00:56 +0330

    some changes 	modified:   Packet++/src/NflogLayer.cpp

--- Packet++/src/NflogLayer.cpp
+++ Packet++/src/NflogLayer.cpp
@@ -17,11 +17,31 @@ namespace pcpp
 
 void NflogLayer::parseNextLayer()
 {
+	if (m_DataLen <= sizeof(nflog_header))
+		return;
+
+	uint8_t* payload = m_Data + sizeof(nflog_header);
+	size_t payloadLen = m_DataLen - sizeof(nflog_header);
+
+	m_NextLayer = static_cast<Layer*>(new IPv4Layer(payload, payloadLen, this, m_Packet));
+
+
 }
 
 void NflogLayer::computeCalculateFields()
 {
-	
+	if (m_NextLayer == NULL)
+		return;
+
+	nflog_header* hdr = getNflogHeader();
+	switch (m_NextLayer->getProtocol())
+	{
+		case IPv4:
+			hdr->address_family = PCPP_ETHERTYPE_IP;
+			break;
+		default:
+			return;
+	}
 }
 
 std::string NflogLayer::toString() const
