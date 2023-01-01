ae4cf2bcfdc8d5ff4fe5fef76ad2b93254ca35b6
Clément Péron <peron.clem@gmail.com>
2023-01-01 15:25:41 +0100

    submit ctest result to CDash

---
+++ CTestConfig.cmake
@@ -0,0 +1,15 @@
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+##
+## # The following are required to submit to the CDash dashboard:
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+
+set(CTEST_PROJECT_NAME "PcapPlusPlus")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
+
+set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=PcapPlusPlus")
+set(CTEST_DROP_SITE_CDASH TRUE)
