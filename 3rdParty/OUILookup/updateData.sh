a24d8afd08a6fc06a469d35c1e9c8597e4c5b3dc
Ege Çetin <egecetin@hotmail.com.tr>
2023-02-05 13:32:18 +0000

    remove whitespace

--- 3rdParty/OUILookup/updateData.sh
+++ 3rdParty/OUILookup/updateData.sh
@@ -10,5 +10,5 @@ python3 createOUIData.py
 # Convert json information to static C file
 mkdir -p include
 rm -f include/PCPP_OUIDataset.h
-echo "#pragma once \n\nstatic const" >> include/PCPP_OUIDataset.h
+echo "#pragma once\n\nstatic const" >> include/PCPP_OUIDataset.h
 xxd -i PCPP_OUIDataset.json >> include/PCPP_OUIDataset.h
