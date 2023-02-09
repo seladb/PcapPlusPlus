0c93205d535a77a5dbfcd4648caa035f6d8a87de
Ege Çetin <egecetin@hotmail.com.tr>
2023-02-09 17:46:42 +0000

    fix pre-commit

--- Common++/header/OUILookup.h
+++ Common++/header/OUILookup.h
@@ -14,8 +14,8 @@ namespace pcpp
 	/**
 	 * @class OUILookup
 	 * Provides vendor name matching functionality from MAC addresses. It uses an internal database to define name of the vendor.
-	 * The class itself should be initialized by using initOUIDatabaseFromJson() otherwise all requests will return "Unknown" as vendor. 
-	 * The class itself currently does not support on-fly modifying the database but anyone who wants to add/modify/remove entries, 
+	 * The class itself should be initialized by using initOUIDatabaseFromJson() otherwise all requests will return "Unknown" as vendor.
+	 * The class itself currently does not support on-fly modifying the database but anyone who wants to add/modify/remove entries,
 	 * should modify 3rdParty/OUILookup/PCPP_OUIDatabase.json file and call to initOUIDatabaseFromJson() function to renew the internal data.
 	 */
 	class OUILookup
