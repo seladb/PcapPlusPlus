8e81157b1c197a24dbc7c2f2cc17c82627ffa4d4
Vivien Voros <wivien19@gmail.com>
2023-02-25 20:53:45 +0100

    COTP fix and doc

--- Packet++/header/CotpLayer.h
+++ Packet++/header/CotpLayer.h
@@ -33,38 +33,85 @@ namespace pcpp {
 	 */
 	class CotpLayer : public Layer {
 	  public:
-		virtual ~CotpLayer() {}
-
-		CotpLayer(uint8_t length, uint8_t pdu_type, uint8_t tpdu_number);
-
+		/**
+		 * Get a pointer to the COTP header. Data can be retrieved through the
+		 * other methods of this layer. Notice the return value points directly to the data, so every change will change the actual packet data
+		 * @return A pointer to the @ref cotphdr
+		 */
 		cotphdr *getCotpHeader() const { return (cotphdr *) m_Data; }
 
+		/**
+		 * @return COTP length
+		 */
 		uint8_t getLength() const;
 
+		/**
+		 * @return COTP PDU type
+		 */
 		uint8_t getPdu_type() const;
 
+		/**
+		 * @return COTP TPDU number
+		 */
 		uint8_t getTpdu_number() const;
 
+		/**
+		 * @return Size of @ref cotphdr
+		 */
 		size_t getHeaderLen() const override {
 			return sizeof(cotphdr);
 		}
 
-		void computeCalculateFields() override;
+		/**
+		 * Does nothing for this layer
+		 */
+		void computeCalculateFields() {}
+
+		/**
+		* Currently identifies the following next layer: S7commLayer
+		*/
+		void parseNextLayer();
+
+		/**
+		 * A static method that takes a byte array and detects whether it is a COTP message
+		 * @param[in] data A byte array
+		 * @param[in] dataSize The byte array size (in bytes)
+		 * @return True if the data is identified as COTP message
+		 */
+		static bool isDataValid(const uint8_t *data, size_t dataSize) {	return data && dataSize; }
+
+		/**
+		 * A static method that checks whether a source or dest port match those associated with the COTP protocol
+		 * @param[in] portSrc Source port number to check
+	 	 * @param[in] portDst Dest port number to check
+	 	 * @return True if the source or dest port match those associated with the COTP protocol
+		 */
+		static bool isCotpPort(uint8_t cotpType) { return cotpType == 0x06 || cotpType == 0xf0; }
+
+		/**
+		 * A method that creates a COTP layer from packet raw data
+		 * @param[in] data A pointer to the raw data
+		 * @param[in] dataLen Size of the data in bytes
+		 * @param[in] prevLayer A pointer to the previous layer
+		 * @param[in] packet A pointer to the Packet instance where layer will be stored
+		 * @return A newly allocated COTP layer
+		 */
+		static CotpLayer *parseCotpLayer(uint8_t *data, size_t dataLen, Layer *prevLayer, Packet *packet);
+
+		/**
+		 * A constructor that creates the layer from an existing packet raw data
+		 * @param[in] data A pointer to the raw data (will be casted to @ref cotphdr)
+		 * @param[in] dataLen Size of the data in bytes
+		 * @param[in] prevLayer A pointer to the previous layer
+		 * @param[in] packet A pointer to the Packet instance where layer will be stored in
+		 */
+		CotpLayer(uint8_t *data, size_t dataLen, Layer *prevLayer, Packet *packet) : Layer(data, dataLen, prevLayer, packet) { m_Protocol = COTP; }
+
+		virtual ~CotpLayer() {}
 
 		std::string toString() const;
 
 		OsiModelLayer getOsiModelLayer() const override { return OsiModelSesionLayer; }
-
-		void parseNextLayer() override;
-
-		static bool isCotpPort(uint8_t cotpType) { return cotpType == 0x06 || cotpType == 0xf0; }
-
-		static CotpLayer *parseCotpLayer(uint8_t *data, size_t dataLen, Layer *prevLayer, Packet *packet);
-
-		CotpLayer();
-
-		CotpLayer(uint8_t *data, size_t dataLen, Layer *prevLayer, Packet *packet) : Layer(data, dataLen, prevLayer,
-																						   packet) { m_Protocol = COTP; }
 	};
 
 
--- Packet++/src/CotpLayer.cpp
+++ Packet++/src/CotpLayer.cpp
@@ -1,30 +1,16 @@
-#include "../header/CotpLayer.h"
+#include "CotpLayer.h"
 #include "EndianPortable.h"
-#include <iostream>
 #include "S7commLayer.h"
-
-#include <cstring>
-#include <sstream>
 #include <PayloadLayer.h>
+#include <cstring>
+#include <iostream>
+#include <sstream>
 
-namespace pcpp {
+namespace pcpp
+{
 
-	pcpp::CotpLayer::CotpLayer(uint8_t length, uint8_t pdu_type, uint8_t tpdu_number) {
-		const size_t headerLen = sizeof(cotphdr);
-		m_DataLen = headerLen;
-		m_Data = new uint8_t[headerLen];
-		memset(m_Data, 0, headerLen);
-		cotphdr *cotpHdr = (cotphdr *) m_Data;
-		cotpHdr->length = length;
-		cotpHdr->pdu_type = pdu_type;
-		cotpHdr->tpdu_number = tpdu_number;
-		m_Protocol = COTP;
-	}
-
-	void CotpLayer::computeCalculateFields() {
-	}
-
-	std::string CotpLayer::toString() const {
+	std::string CotpLayer::toString() const
+	{
 		std::ostringstream lengthStream;
 		lengthStream << getLength();
 		std::ostringstream pduTypeStream;
@@ -32,12 +18,12 @@ namespace pcpp {
 		std::ostringstream tpduNumberStream;
 		tpduNumberStream << getTpdu_number();
 
-		return "Cotp Layer, length: " + lengthStream.str() +
-			   ", pdu_type: " + pduTypeStream.str() +
+		return "COTP Layer, length: " + lengthStream.str() + ", pdu_type: " + pduTypeStream.str() +
 			   ", tpdu_number: " + tpduNumberStream.str();
 	}
 
-	void CotpLayer::parseNextLayer() {
+	void CotpLayer::parseNextLayer()
+	{
 		size_t headerLen = getHeaderLen();
 		if (m_DataLen <= headerLen)
 			return;
@@ -45,45 +31,24 @@ namespace pcpp {
 		uint8_t *payload = m_Data + headerLen;
 		size_t payloadLen = m_DataLen - headerLen;
 
-		if (S7commLayer::isS7commPort(payload[0])) {
+		if (S7commLayer::isS7commPort(payload[0]) && S7commLayer::isDataValid(payload, payloadLen))
+		{
 
 			m_NextLayer = S7commLayer::parseS7commLayer(payload, payloadLen, this, m_Packet);
-
-		} else
+		}
+		else
 			m_NextLayer = new PayloadLayer(payload, payloadLen, this, m_Packet);
-
 	}
 
-	CotpLayer *CotpLayer::parseCotpLayer(uint8_t *data, size_t dataLen, Layer *prevLayer, Packet *packet) {
-		if (dataLen < sizeof(cotphdr))
-			return NULL;
-
-		cotphdr *cotpHdr = (cotphdr *) data;
-
-		// illegal header data - length is too small
-		if (be16toh(cotpHdr->length) < static_cast<uint16_t>(sizeof(cotphdr)))
-			return NULL;
+	CotpLayer *CotpLayer::parseCotpLayer(uint8_t *data, size_t dataLen, Layer *prevLayer, Packet *packet)
+	{
 		return new CotpLayer(data, dataLen, prevLayer, packet);
 	}
 
-	CotpLayer::CotpLayer() {
-		const size_t headerLen = sizeof(cotphdr);
-		m_DataLen = headerLen;
-		m_Data = new uint8_t[headerLen];
-		memset(m_Data, 0, headerLen);
-		m_Protocol = COTP;
-	}
+	uint8_t CotpLayer::getLength() const { return getCotpHeader()->length; }
 
-	uint8_t CotpLayer::getLength() const {
-		return getCotpHeader()->length;
-	}
+	uint8_t CotpLayer::getPdu_type() const { return getCotpHeader()->pdu_type; }
 
-	uint8_t CotpLayer::getPdu_type() const {
-		return getCotpHeader()->pdu_type;
-	}
-
-	uint8_t CotpLayer::getTpdu_number() const {
-		return getCotpHeader()->tpdu_number;
-	}
+	uint8_t CotpLayer::getTpdu_number() const { return getCotpHeader()->tpdu_number; }
 
 } // namespace pcpp
