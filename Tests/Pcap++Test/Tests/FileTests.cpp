391fa763f5c820bf842b6442cd4d6018495c63ec
Ege Çetin <64282645+egecetin@users.noreply.github.com>
2023-03-10 11:03:50 +0300

    Check packetCount for FileTests (#1095)

--- Tests/Pcap++Test/Tests/FileTests.cpp
+++ Tests/Pcap++Test/Tests/FileTests.cpp
@@ -77,6 +77,7 @@ PTF_TEST_CASE(TestPcapFileReadWrite)
 	PTF_ASSERT_EQUAL((uint32_t)writerStatistics.packetsRecv, 4631);
 	PTF_ASSERT_EQUAL((uint32_t)writerStatistics.packetsDrop, 0);
 
+	PTF_ASSERT_EQUAL(packetCount, 4631);
 	PTF_ASSERT_EQUAL(ethCount, 4631);
 	PTF_ASSERT_EQUAL(sllCount, 0);
 	PTF_ASSERT_EQUAL(ipCount, 4631);
@@ -146,6 +147,7 @@ PTF_TEST_CASE(TestPcapSllFileReadWrite)
 	PTF_ASSERT_EQUAL((uint32_t)writerStatistics.packetsRecv, 518);
 	PTF_ASSERT_EQUAL((uint32_t)writerStatistics.packetsDrop, 0);
 
+	PTF_ASSERT_EQUAL(packetCount, 518);
 	PTF_ASSERT_EQUAL(ethCount, 0);
 	PTF_ASSERT_EQUAL(sllCount, 518);
 	PTF_ASSERT_EQUAL(ipCount, 510);
@@ -212,6 +214,7 @@ PTF_TEST_CASE(TestPcapRawIPFileReadWrite)
 	PTF_ASSERT_EQUAL((uint32_t)writerNgStatistics.packetsRecv, 100);
 	PTF_ASSERT_EQUAL((uint32_t)writerNgStatistics.packetsDrop, 0);
 
+	PTF_ASSERT_EQUAL(packetCount, 100);
 	PTF_ASSERT_EQUAL(ethCount, 0);
 	PTF_ASSERT_EQUAL(ipv4Count, 50);
 	PTF_ASSERT_EQUAL(ipv6Count, 50);
@@ -336,6 +339,7 @@ PTF_TEST_CASE(TestPcapNgFileReadWrite)
 	PTF_ASSERT_EQUAL((uint32_t)writerStatistics.packetsRecv, 64);
 	PTF_ASSERT_EQUAL((uint32_t)writerStatistics.packetsDrop, 0);
 
+	PTF_ASSERT_EQUAL(packetCount, 64);
 	PTF_ASSERT_EQUAL(ethLinkLayerCount, 62);
 	PTF_ASSERT_EQUAL(nullLinkLayerCount, 2);
 	PTF_ASSERT_EQUAL(otherLinkLayerCount, 0);
@@ -709,6 +713,7 @@ PTF_TEST_CASE(TestPcapFileReadLinkTypeIPv6)
 	PTF_ASSERT_EQUAL((uint32_t)readerStatistics.packetsRecv, 1);
 	PTF_ASSERT_EQUAL((uint32_t)readerStatistics.packetsDrop, 0);
 
+	PTF_ASSERT_EQUAL(packetCount, 1);
 	PTF_ASSERT_EQUAL(ethCount, 0);
 	PTF_ASSERT_EQUAL(ipCount, 1);
 	PTF_ASSERT_EQUAL(tcpCount, 1);
@@ -752,6 +757,7 @@ PTF_TEST_CASE(TestPcapFileReadLinkTypeIPv4)
 	PTF_ASSERT_EQUAL((uint32_t)readerStatistics.packetsRecv, 2);
 	PTF_ASSERT_EQUAL((uint32_t)readerStatistics.packetsDrop, 0);
 
+	PTF_ASSERT_EQUAL(packetCount, 2);
 	PTF_ASSERT_EQUAL(ethCount, 0);
 	PTF_ASSERT_EQUAL(ipCount, 2);
 	PTF_ASSERT_EQUAL(tcpCount, 0);
@@ -797,6 +803,7 @@ PTF_TEST_CASE(TestSolarisSnoopFileRead)
 	PTF_ASSERT_EQUAL((uint32_t)readerStatistics.packetsRecv, 250);
 	PTF_ASSERT_EQUAL((uint32_t)readerStatistics.packetsDrop, 0);
 
+	PTF_ASSERT_EQUAL(packetCount, 250);
 	PTF_ASSERT_EQUAL(ethCount, 142);
 	PTF_ASSERT_EQUAL(ethDot3Count, 108);
 	PTF_ASSERT_EQUAL(ipCount, 71);
