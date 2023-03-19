e9ce7ada42bb4f60f451f155ff04b2e4f97cda74
traversebitree <xykong421@outlook.com>
2023-03-20 01:13:59 +0800

    Recover to default (Default is -MDd)

--- Tests/Packet++Test/main.cpp
+++ Tests/Packet++Test/main.cpp
@@ -83,7 +83,7 @@ int main(int argc, char* argv[])
 	std::cout << "Disabling memory leak check in MSVC Release builds due to caching logic in stream objects that looks like a memory leak:" << std::endl
 	<< "     https://github.com/cpputest/cpputest/issues/786#issuecomment-148921958" << std::endl;
 	#endif
-	
+
 	// The logger singleton looks like a memory leak. Invoke it before starting the memory check
 	pcpp::Logger::getInstance();
 
