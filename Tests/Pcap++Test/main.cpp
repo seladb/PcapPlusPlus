24b63a0d879e32eadadd70ffb90505616a6b06b0
traversebitree <xykong421@outlook.com>
2023-03-19 14:25:10 +0800

    Discard memplumber and test(no overload new/delete)

--- Tests/Pcap++Test/main.cpp
+++ Tests/Pcap++Test/main.cpp
@@ -63,7 +63,7 @@ int main(int argc, char* argv[])
 	std::string userTagsInclude = "", userTagsExclude = "", configTags = "";
 	bool runWithNetworking = true;
 	bool memVerbose = false;
-	bool skipMemLeakCheck = false;
+	bool skipMemLeakCheck = true;
 
 	int optionIndex = 0;
 	int opt = 0;
