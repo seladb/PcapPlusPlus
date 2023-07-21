81492258f4b378031b851f22f738fc228c6db8a0
Alex <aleksandrosansan@gmail.com>
2023-07-21 12:50:11 +0000

    Remove trailing whitespace

--- Packet++/header/TLVData.h
+++ Packet++/header/TLVData.h
@@ -260,7 +260,7 @@ namespace pcpp
 		TLVRecordType getFirstTLVRecord(uint8_t* tlvDataBasePtr, size_t tlvDataLen) const
 		{
 			TLVRecordType resRec(tlvDataBasePtr); // for NRVO optimization
-			
+
 			// resRec pointer is out-bounds of the TLV records memory
 			if (resRec.getRecordBasePtr() + resRec.getTotalSize() > tlvDataBasePtr + tlvDataLen)
 				resRec.assign(NULL);
