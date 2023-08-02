36a4e2cae4b2b2f8c38fbed7d28494dfac283591
Alex <aleksandrosansan@gmail.com>
2023-08-02 08:47:57 +0200

    Fix Heap-buffer-overflow (read) in NflogTlv::getType (#1163)

--- Packet++/header/TLVData.h
+++ Packet++/header/TLVData.h
@@ -259,11 +259,14 @@ namespace pcpp
 		 */
 		TLVRecordType getFirstTLVRecord(uint8_t* tlvDataBasePtr, size_t tlvDataLen) const
 		{
-			// In most cases tlvDataLen is not zero and the size is correct therefore the overhead is not significant if the checks will be done later
 			TLVRecordType resRec(tlvDataBasePtr); // for NRVO optimization
 
+			// resRec pointer is out-bounds of the TLV records memory
+			if (resRec.getRecordBasePtr() + resRec.getTotalSize() > tlvDataBasePtr + tlvDataLen)
+				resRec.assign(NULL);
+
 			// check if there are records at all and the total size is not zero
-			if (tlvDataLen == 0 || resRec.getTotalSize() == 0)
+			if (!resRec.isNull() && (tlvDataLen == 0 || resRec.getTotalSize() == 0))
 				resRec.assign(NULL);
 
 			return resRec;
