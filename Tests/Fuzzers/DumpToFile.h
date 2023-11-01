ff23ca3e43c15aa7b2c59b72baa0a8e94d82533a
Alex <aleksandrosansan@gmail.com>
2023-11-01 20:50:21 +0000

    Improve fuzzing coverage

---
+++ Tests/Fuzzers/DumpToFile.h
@@ -0,0 +1,32 @@
+#ifndef DUMP_TO_FILE_H
+#define DUMP_TO_FILE_H
+
+#include <iostream>
+
+// This function is created as PcapPlusPlus doesn't seem to offer a way of
+// parsing Pcap files directly from memory
+int dumpDataToPcapFile(const uint8_t *data, size_t size, const char* path)
+{
+	FILE *fd;
+	int written = 0;
+
+	fd = fopen(path, "wb");
+	if (fd == NULL)
+	{
+		std::cerr << "Error opening pcap file for writing\n";
+		return -1;
+	}
+
+	written = fwrite(data, 1, size, fd);
+	if (static_cast<size_t>(written) != size)
+	{
+		std::cerr << "Error writing pcap file\n";
+		fclose(fd);
+		return -1;
+	}
+
+	fclose(fd);
+	return 0;
+}
+
+#endif // DUMP_TO_FILE_H
