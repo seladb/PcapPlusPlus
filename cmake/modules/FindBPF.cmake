805043e0c3415425cf46c35a17d16878f83c5a07
seladb <pcapplusplus@gmail.com>
2023-11-07 00:06:40 -0800

    Add `FindBPF`

---
+++ cmake/modules/FindBPF.cmake
@@ -0,0 +1,34 @@
+# - Try to find libbpf
+#
+# Once done this will define
+#  BPF_FOUND        - System has libbpf
+#  BPF_INCLUDE_DIRS - The libbpf include directories
+#  BPF_LIBRARIES    - The libraries needed to use libbpf
+
+find_package(PkgConfig QUIET)
+pkg_check_modules(PC_BPF libbpf)
+
+find_path(BPF_INCLUDE_DIR
+  NAMES bpf/bpf.h
+  HINTS ${PC_BPF_INCLUDE_DIRS})
+
+find_library(
+  BPF_LIBRARY
+  NAMES bpf
+  HINTS ${PC_BPF_LIBRARY_DIRS})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+  BPF
+  REQUIRED_VARS BPF_LIBRARY BPF_INCLUDE_DIR
+  VERSION_VAR BPF_VERSION
+  FAIL_MESSAGE "libbpf not found!")
+
+if(BPF_FOUND AND NOT TARGET BPF::BPF)
+  add_library(BPF::BPF INTERFACE IMPORTED)
+  set_target_properties(
+    BPF::BPF
+    PROPERTIES INTERFACE_LINK_LIBRARIES "${BPF_LIBRARIES}"
+    INTERFACE_INCLUDE_DIRECTORIES "${BPF_INCLUDE_DIRS}"
+    INTERFACE_COMPILE_OPTIONS "${BPF_CFLAGS_OTHER}")
+endif()
