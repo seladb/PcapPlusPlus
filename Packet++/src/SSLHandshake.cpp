f9750a4c11f7aef599d5a3251dc6eabd0017aa7e
Alex <aleksandrosansan@gmail.com>
2023-11-13 01:56:02 +0100

    Fix oss-fuzz issue 64124
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=64124 (#1238)

--- Packet++/src/SSLHandshake.cpp
+++ Packet++/src/SSLHandshake.cpp
@@ -1402,7 +1402,7 @@ uint16_t SSLClientHelloMessage::getCipherSuiteID(int index, bool& isValid) const
 	}
 
 	size_t cipherSuiteStartOffset = sizeof(ssl_tls_client_server_hello) + sizeof(uint8_t) + getSessionIDLength() + sizeof(uint16_t);
-	if (cipherSuiteStartOffset + sizeof(uint16_t) > m_DataLen)
+	if (cipherSuiteStartOffset + sizeof(uint16_t) * (index + 1) > m_DataLen)
 	{
 		isValid = false;
 		return 0;
