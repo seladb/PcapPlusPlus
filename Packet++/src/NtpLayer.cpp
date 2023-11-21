5f1895a7b496fa78a2927bd5fe610ab619a3f49b
Alex <aleksandrosansan@gmail.com>
2023-11-21 09:34:28 +0100

    Fix oss-fuzz issue 64237 (#1244)
    Fix https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=64237

--- Packet++/src/NtpLayer.cpp
+++ Packet++/src/NtpLayer.cpp
@@ -249,8 +249,8 @@ namespace pcpp
                     return "A step change in system time has occurred, but the association has not yet resynchronized";
                 default:
                 {
-                    char arrBuff[4] = {static_cast<char>((refID >> 24) & 0xFF), static_cast<char>((refID >> 16) & 0xFF),
-                                       static_cast<char>((refID >> 8) & 0xFF), static_cast<char>((refID) & 0xFF)};
+                    char arrBuff[5] = {static_cast<char>((refID >> 24) & 0xFF), static_cast<char>((refID >> 16) & 0xFF),
+                                       static_cast<char>((refID >> 8) & 0xFF), static_cast<char>((refID) & 0xFF), '\0'};
                     return arrBuff;
                 }
                 }
