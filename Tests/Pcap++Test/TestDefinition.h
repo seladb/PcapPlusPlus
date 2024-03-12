1ff377c5de22caf9907fcc31f3f71ea138346155
Axel Mahr <115995876+axmahr@users.noreply.github.com>
2024-03-12 18:56:23 +0100

    Handle too high interface ID for pcapng (Issue #1334) (#1337)

--- Tests/Pcap++Test/TestDefinition.h
+++ Tests/Pcap++Test/TestDefinition.h
@@ -24,6 +24,7 @@ PTF_TEST_CASE(TestPcapRawIPFileReadWrite);
 PTF_TEST_CASE(TestPcapFileAppend);
 PTF_TEST_CASE(TestPcapNgFileReadWrite);
 PTF_TEST_CASE(TestPcapNgFileReadWriteAdv);
+PTF_TEST_CASE(TestPcapNgFileTooManyInterfaces);
 PTF_TEST_CASE(TestPcapFileReadLinkTypeIPv6);
 PTF_TEST_CASE(TestPcapFileReadLinkTypeIPv4);
 PTF_TEST_CASE(TestSolarisSnoopFileRead);
