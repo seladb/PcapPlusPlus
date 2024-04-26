b99b698692ecf3cb6084207d611e95325686bc7e
Dimitar Krastev <dimi1010100@gmail.com>
2024-04-26 15:27:19 +0300

    Added backport of Cpp14's make_unique.

---
+++ Common++/header/MemoryUtils.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <cstddef>
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+/// @file
+
+namespace pcpp
+{
+	namespace detail
+	{
+		// Backports of std methods that are post-Cpp11.
+		namespace stdbackports
+		{
+			template <class T> struct _Unique_if
+			{
+				typedef std::unique_ptr<T> _Single_object;
+			};
+
+			template <class T> struct _Unique_if<T[]>
+			{
+				typedef std::unique_ptr<T[]> _Unknown_bound;
+			};
+
+			template <class T, size_t N> struct _Unique_if<T[N]>
+			{
+				typedef void _Known_bound;
+			};
+
+			template <class T, class... Args> typename _Unique_if<T>::_Single_object make_unique(Args &&...args)
+			{
+				return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+			}
+
+			template <class T> typename _Unique_if<T>::_Unknown_bound make_unique(size_t n)
+			{
+				typedef typename std::remove_extent<T>::type U;
+				return std::unique_ptr<T>(new U[n]());
+			}
+
+			template <class T, class... Args> typename _Unique_if<T>::_Known_bound make_unique(Args &&...) = delete;
+		}
+	}
+}
