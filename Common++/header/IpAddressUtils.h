e18ae9603eec339e29ad24f0841bbe9206188847
Dimitar Krastev <dimi1010100@gmail.com>
2024-05-06 17:18:30 +0300

    Inlined equality overloads.

--- Common++/header/IpAddressUtils.h
+++ Common++/header/IpAddressUtils.h
@@ -12,22 +12,22 @@
 namespace pcpp
 {
 	bool operator==(const IPv4Address& lhs, const in_addr& rhs);
-	bool operator!=(const IPv4Address& lhs, const in_addr& rhs) { return !(lhs == rhs); }
-	bool operator==(const in_addr& lhs, const IPv4Address& rhs) { return rhs == lhs; }
-	bool operator!=(const in_addr& lhs, const IPv4Address& rhs) { return !(lhs == rhs); }
+	inline bool operator!=(const IPv4Address& lhs, const in_addr& rhs) { return !(lhs == rhs); }
+	inline bool operator==(const in_addr& lhs, const IPv4Address& rhs) { return rhs == lhs; }
+	inline bool operator!=(const in_addr& lhs, const IPv4Address& rhs) { return !(lhs == rhs); }
 
 	bool operator==(const IPv6Address& lhs, const in6_addr& rhs);
-	bool operator!=(const IPv6Address& lhs, const in6_addr& rhs) { return !(lhs == rhs); }
-	bool operator==(const in6_addr& lhs, const IPv6Address& rhs) { return rhs == lhs; }
-	bool operator!=(const in6_addr& lhs, const IPv6Address& rhs) { return !(lhs == rhs); }
+	inline bool operator!=(const IPv6Address& lhs, const in6_addr& rhs) { return !(lhs == rhs); }
+	inline bool operator==(const in6_addr& lhs, const IPv6Address& rhs) { return rhs == lhs; }
+	inline bool operator!=(const in6_addr& lhs, const IPv6Address& rhs) { return !(lhs == rhs); }
 		
 	bool operator==(const IPAddress& lhs, const in_addr& rhs);
-	bool operator!=(const IPAddress& lhs, const in_addr& rhs) { return !(lhs == rhs); }
-	bool operator==(const in_addr& lhs, const IPAddress &rhs) { return rhs == lhs; }
-	bool operator!=(const in_addr& lhs, const IPAddress &rhs) { return !(lhs == rhs); }
+	inline bool operator!=(const IPAddress& lhs, const in_addr& rhs) { return !(lhs == rhs); }
+	inline bool operator==(const in_addr& lhs, const IPAddress& rhs) { return rhs == lhs; }
+	inline bool operator!=(const in_addr& lhs, const IPAddress& rhs) { return !(lhs == rhs); }
 
 	bool operator==(const IPAddress& lhs, const in6_addr& rhs);
-	bool operator!=(const IPAddress& lhs, const in6_addr& rhs) { return !(lhs == rhs); }
-	bool operator==(const in6_addr& lhs, const IPAddress& rhs) { return rhs == lhs; }
-	bool operator!=(const in6_addr& lhs, const IPAddress& rhs) { return !(lhs == rhs); }
+	inline bool operator!=(const IPAddress& lhs, const in6_addr& rhs) { return !(lhs == rhs); }
+	inline bool operator==(const in6_addr& lhs, const IPAddress& rhs) { return rhs == lhs; }
+	inline bool operator!=(const in6_addr& lhs, const IPAddress& rhs) { return !(lhs == rhs); }
 }
