50099eeabf85e3739eb8bf4f6e157cd625acf6a1
Dimitar Krastev <dimi1010100@gmail.com>
2024-05-06 17:12:39 +0300

    Added comparison operators between IPAddress types and in_addr/in6_addr.

---
+++ Common++/header/IpAddressUtils.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "IpAddress.h"
+#include "IpUtils.h" // Just needing in_addr and in6_addr.
+
+/// @file
+
+/**
+ * \namespace pcpp
+ * \brief The main namespace for the PcapPlusPlus lib
+ */
+namespace pcpp
+{
+	bool operator==(const IPv4Address& lhs, const in_addr& rhs);
+	bool operator!=(const IPv4Address& lhs, const in_addr& rhs) { return !(lhs == rhs); }
+	bool operator==(const in_addr& lhs, const IPv4Address& rhs) { return rhs == lhs; }
+	bool operator!=(const in_addr& lhs, const IPv4Address& rhs) { return !(lhs == rhs); }
+
+	bool operator==(const IPv6Address& lhs, const in6_addr& rhs);
+	bool operator!=(const IPv6Address& lhs, const in6_addr& rhs) { return !(lhs == rhs); }
+	bool operator==(const in6_addr& lhs, const IPv6Address& rhs) { return rhs == lhs; }
+	bool operator!=(const in6_addr& lhs, const IPv6Address& rhs) { return !(lhs == rhs); }
+		
+	bool operator==(const IPAddress& lhs, const in_addr& rhs);
+	bool operator!=(const IPAddress& lhs, const in_addr& rhs) { return !(lhs == rhs); }
+	bool operator==(const in_addr& lhs, const IPAddress &rhs) { return rhs == lhs; }
+	bool operator!=(const in_addr& lhs, const IPAddress &rhs) { return !(lhs == rhs); }
+
+	bool operator==(const IPAddress& lhs, const in6_addr& rhs);
+	bool operator!=(const IPAddress& lhs, const in6_addr& rhs) { return !(lhs == rhs); }
+	bool operator==(const in6_addr& lhs, const IPAddress& rhs) { return rhs == lhs; }
+	bool operator!=(const in6_addr& lhs, const IPAddress& rhs) { return !(lhs == rhs); }
+}
