b95ad5f3c7a22276f9103d340cc18f17d09380aa
Ege Çetin <egecetin@hotmail.com.tr>
2024-05-15 09:53:52 +0300

    Remove unnecessary ternary

--- Tests/Pcap++Test/Tests/FileTests.cpp
+++ Tests/Pcap++Test/Tests/FileTests.cpp
@@ -133,7 +133,7 @@ PTF_TEST_CASE(TestPcapFilePrecision)
 	pcpp::RawPacket readPacketNano, readPacketMicro;
 	PTF_ASSERT_TRUE(readerDevNano.getNextPacket(readPacketMicro));
 	PTF_ASSERT_EQUAL(readPacketMicro.getPacketTimeStamp().tv_sec, 1);
-	PTF_ASSERT_EQUAL(readPacketMicro.getPacketTimeStamp().tv_nsec, pcpp::PcapFileReaderDevice::isTimestampPrecisionNanoSupported() ? 2000 : 2000);
+	PTF_ASSERT_EQUAL(readPacketMicro.getPacketTimeStamp().tv_nsec, 2000);
 
 	PTF_ASSERT_TRUE(readerDevNano.getNextPacket(readPacketNano));
 	PTF_ASSERT_EQUAL(readPacketNano.getPacketTimeStamp().tv_sec, 1);
@@ -148,11 +148,11 @@ PTF_TEST_CASE(TestPcapFilePrecision)
 
 	PTF_ASSERT_TRUE(readerDevMicro.getNextPacket(readPacketMicro2));
 	PTF_ASSERT_EQUAL(readPacketMicro2.getPacketTimeStamp().tv_sec, 1);
-	PTF_ASSERT_EQUAL(readPacketMicro2.getPacketTimeStamp().tv_nsec, pcpp::PcapFileReaderDevice::isTimestampPrecisionNanoSupported() ? 2000 : 2000);
+	PTF_ASSERT_EQUAL(readPacketMicro2.getPacketTimeStamp().tv_nsec, pcpp::2000);
 
 	PTF_ASSERT_TRUE(readerDevMicro.getNextPacket(readPacketNano2));
 	PTF_ASSERT_EQUAL(readPacketNano2.getPacketTimeStamp().tv_sec, 1);
-	PTF_ASSERT_EQUAL(readPacketNano2.getPacketTimeStamp().tv_nsec, pcpp::PcapFileReaderDevice::isTimestampPrecisionNanoSupported() ? 1000 : 1000);
+	PTF_ASSERT_EQUAL(readPacketNano2.getPacketTimeStamp().tv_nsec, 1000);
 
 	readerDevMicro.close();
 } // TestPcapFilePrecision
