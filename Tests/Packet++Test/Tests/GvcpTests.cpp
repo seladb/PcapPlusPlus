f350012a298c092ef949fa4fc89f1fa1d4348cb2
anchi.liu <anchi.liu@mujin.co.jp>
2024-05-28 17:54:15 +0900

    fix test

--- Tests/Packet++Test/Tests/GvcpTests.cpp
+++ Tests/Packet++Test/Tests/GvcpTests.cpp
@@ -7,10 +7,8 @@ PTF_TEST_CASE(GvcpBasicTest)
 {
 	using namespace pcpp;
 
-	// create a unique pointer of a buffer
-	std::vector<uint8_t> payload = {0x00, 0x01, 0x02, 0x03};
-
 	{
+		std::vector<uint8_t> payload = {0x00, 0x01, 0x02, 0x03};
 		GvcpRequestLayer gvcpRequestLayer(GvcpCommand::DiscoveredCmd, payload.data(), payload.size(), 1, 2);
 		PTF_ASSERT_EQUAL(gvcpRequestLayer.getProtocol(), Gvcp);
 
@@ -22,6 +20,7 @@ PTF_TEST_CASE(GvcpBasicTest)
 		PTF_ASSERT_EQUAL(header->dataSize, payload.size());
 	}
 	{
+		std::vector<uint8_t> payload = {0x00, 0x01, 0x02, 0x03};
 		GvcpAcknowledgeLayer gvcpAcknowledgeLayer(GvcpResponseStatus::Success, GvcpCommand::DiscoveredAck,
 												  payload.data(), payload.size(), 2);
 		PTF_ASSERT_EQUAL(gvcpAcknowledgeLayer.getProtocol(), Gvcp);
@@ -31,6 +30,7 @@ PTF_TEST_CASE(GvcpBasicTest)
 		PTF_ASSERT_EQUAL(uint16_t(header->command), uint16_t(GvcpCommand::DiscoveredAck));
 		PTF_ASSERT_EQUAL(header->ackId, 2);
 		PTF_ASSERT_EQUAL(header->dataSize, payload.size());
+
 	}
 }
 
@@ -56,8 +56,8 @@ PTF_TEST_CASE(GvcpDiscoveryAck)
 		PTF_ASSERT_TRUE(header != nullptr);
 		PTF_ASSERT_EQUAL(uint16_t(header->status), uint16_t(GvcpResponseStatus::Success));
 		PTF_ASSERT_EQUAL(uint16_t(header->command), uint16_t(GvcpCommand::DiscoveredAck));
-		PTF_ASSERT_EQUAL(header->ackId, 2);
-		PTF_ASSERT_EQUAL(header->dataSize, dataLen);
+		PTF_ASSERT_EQUAL(header->ackId, 1);
+		PTF_ASSERT_EQUAL(header->dataSize, dataLen - sizeof(GvcpAckHeader));
 
 		auto discoveryBody = gvcpAcknowledgeLayer.getGvcpDiscoveryBody();
 		PTF_ASSERT_TRUE(discoveryBody != nullptr);
