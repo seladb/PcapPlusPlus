9a5e63897c4dc8e8a06feeb6b630649106786581
Dimitar Krastev <dimi1010100@gmail.com>
2024-06-06 21:29:39 +0300

    Moved getAllRemotePcapDevices to anonymous namespace in PcapRemoteDeviceList.cpp

--- Pcap++/src/DeviceUtils.cpp
+++ Pcap++/src/DeviceUtils.cpp
@@ -23,28 +23,5 @@ namespace pcpp
 			// Assigns the raw pointer to the smart pointer with specialized deleter.
 			return std::unique_ptr<pcap_if_t, internal::PcapFreeAllDevsDeleter>(interfaceListRaw);
 		}
-
-#ifdef _WIN32
-		std::unique_ptr<pcap_if_t, PcapFreeAllDevsDeleter> getAllRemotePcapDevices(const IPAddress& ipAddress, uint16_t port, pcap_rmtauth* pRmAuth)
-		{
-			PCPP_LOG_DEBUG("Searching remote devices on IP: " << ipAddress << " and port: " << port);
-			std::array<char, PCAP_BUF_SIZE> remoteCaptureString;
-			std::array<char, PCAP_ERRBUF_SIZE> errorBuf;
-			if (pcap_createsrcstr(remoteCaptureString.data(), PCAP_SRC_IFREMOTE, ipAddress.toString().c_str(),
-								  std::to_string(port).c_str(), nullptr, errorBuf.data()) != 0)
-			{
-				throw std::runtime_error("Error creating the remote connection string. Error: " + std::string(errorBuf.begin(), errorBuf.end()));
-			}
-
-			PCPP_LOG_DEBUG("Remote capture string: " << remoteCaptureString.data());
-
-			pcap_if_t* interfaceListRaw;
-			if (pcap_findalldevs_ex(remoteCaptureString.data(), pRmAuth, &interfaceListRaw, errorBuf.data()) < 0)
-			{
-				throw std::runtime_error("Error retrieving device on remote machine. Error: " + std::string(errorBuf.begin(), errorBuf.end()));
-			}
-			return std::unique_ptr<pcap_if_t, internal::PcapFreeAllDevsDeleter>(interfaceListRaw);
-		}
-#endif // _WIN32
 	}
 }
--- Pcap++/src/PcapRemoteDeviceList.cpp
+++ Pcap++/src/PcapRemoteDeviceList.cpp
@@ -5,7 +5,6 @@
 #include "PcapRemoteDeviceList.h"
 #include "Logger.h"
 #include "IpUtils.h"
-#include "DeviceUtils.h"
 #include "PcapUtils.h"
 #include "IpAddressUtils.h"
 #include "pcap.h"
@@ -14,6 +13,40 @@
 
 namespace pcpp
 {
+
+	namespace
+	{
+		/**
+		 * Fetches a list of all network devices on a remote machine that WinPcap/NPcap can find.
+		 * @param[in] ipAddress IP address of the remote machine.
+		 * @param[in] port Port to use when connecting to the remote machine.
+		 * @param[in] pRmAuth Pointer to an authentication structure to use when connecting to the remote machine. Nullptr if no authentication is required.
+		 * @return A smart pointer to an interface list structure.
+		 * @throws std::runtime_error The system encountered an error fetching the devices.
+		 */
+		std::unique_ptr<pcap_if_t, internal::PcapFreeAllDevsDeleter> getAllRemotePcapDevices(const IPAddress& ipAddress, uint16_t port, pcap_rmtauth* pRmAuth = nullptr)
+		{
+			PCPP_LOG_DEBUG("Searching remote devices on IP: " << ipAddress << " and port: " << port);
+			std::array<char, PCAP_BUF_SIZE> remoteCaptureString;
+			std::array<char, PCAP_ERRBUF_SIZE> errorBuf;
+			if (pcap_createsrcstr(remoteCaptureString.data(), PCAP_SRC_IFREMOTE, ipAddress.toString().c_str(),
+								  std::to_string(port).c_str(), nullptr, errorBuf.data()) != 0)
+			{
+				throw std::runtime_error("Error creating the remote connection string. Error: " +
+										 std::string(errorBuf.begin(), errorBuf.end()));
+			}
+
+			PCPP_LOG_DEBUG("Remote capture string: " << remoteCaptureString.data());
+
+			pcap_if_t* interfaceListRaw;
+			if (pcap_findalldevs_ex(remoteCaptureString.data(), pRmAuth, &interfaceListRaw, errorBuf.data()) < 0)
+			{
+				throw std::runtime_error("Error retrieving device on remote machine. Error: " +
+										 std::string(errorBuf.begin(), errorBuf.end()));
+			}
+			return std::unique_ptr<pcap_if_t, internal::PcapFreeAllDevsDeleter>(interfaceListRaw);
+		}
+	}
 
 PcapRemoteDeviceList* PcapRemoteDeviceList::getRemoteDeviceList(const IPAddress& ipAddress, uint16_t port)
 {
@@ -34,7 +67,7 @@ PcapRemoteDeviceList* PcapRemoteDeviceList::getRemoteDeviceList(const IPAddress&
 	std::unique_ptr<pcap_if_t, internal::PcapFreeAllDevsDeleter> interfaceList;
 	try
 	{
-		interfaceList = internal::getAllRemotePcapDevices(ipAddress, port, pRmAuth);
+		interfaceList = getAllRemotePcapDevices(ipAddress, port, pRmAuth);
 	}
 	catch (const std::exception& e)
 	{
