70bb739849073f9c31e6e7a9dca3c586dc1a4427
Dimitar Krastev <dimi1010100@gmail.com>
2024-06-06 23:39:54 +0300

    Changed shared remote authentication structure to be managed by a shared pointer.

--- Pcap++/header/PcapRemoteDeviceList.h
+++ Pcap++/header/PcapRemoteDeviceList.h
@@ -2,6 +2,7 @@
 
 #if defined(_WIN32)
 
+#include <memory>
 #include "IpAddress.h"
 #include "PcapRemoteDevice.h"
 
@@ -30,10 +31,10 @@ namespace pcpp
 		std::vector<PcapRemoteDevice*> m_RemoteDeviceList;
 		IPAddress m_RemoteMachineIpAddress;
 		uint16_t m_RemoteMachinePort;
-		PcapRemoteAuthentication* m_RemoteAuthentication;
+		std::shared_ptr<PcapRemoteAuthentication> m_RemoteAuthentication;
 
 		// private c'tor. User should create the list via static methods PcapRemoteDeviceList::getRemoteDeviceList()
-		PcapRemoteDeviceList() : m_RemoteMachinePort(0), m_RemoteAuthentication(NULL) {}
+		PcapRemoteDeviceList() : m_RemoteMachinePort(0), m_RemoteAuthentication(nullptr) {}
 
 		void setRemoteMachineIpAddress(const IPAddress& ipAddress);
 		void setRemoteMachinePort(uint16_t port);
