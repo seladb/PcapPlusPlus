37de157b9e898606a2fa7b6bd31843044558f36e
Dimitar Krastev <dimi1010100@gmail.com>
2024-06-24 22:36:56 +0300

    Refactored check-check-clang-format-version to a python script to fix crashes on win32.

---
+++ ci/check-clang-format-version.py
@@ -0,0 +1,20 @@
+import subprocess
+
+EXPECTED_CLANG_VERSION = '18.1.6'
+
+
+def main():
+    result = subprocess.run('clang-format --version', capture_output=True)
+    result.check_returncode()
+
+    version_str = result.stdout.decode('utf-8').split(' ')[2].strip()
+    if version_str != EXPECTED_CLANG_VERSION:
+        print(f'Error: Found clang-format version {version_str}, but {EXPECTED_CLANG_VERSION} is required.')
+        exit(1)
+
+    print('Clang format version satisfied.')
+    exit(0)
+
+
+if __name__ == '__main__':
+    main()
