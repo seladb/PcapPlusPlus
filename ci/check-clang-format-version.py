de4d3d8565ddab43fc6a75b2012d54bff20491d5
Dimitar Krastev <dimi1010100@gmail.com>
2024-06-25 12:21:04 +0300

    Changed string split to use default whitespace separators.

--- ci/check-clang-format-version.py
+++ ci/check-clang-format-version.py
@@ -7,7 +7,7 @@ def main():
     result = subprocess.run(("clang-format", "--version"), capture_output=True)
     result.check_returncode()
 
-    version_str = result.stdout.decode("utf-8").split(" ")[2].strip()
+    version_str = result.stdout.decode("utf-8").split()[2].strip()
     if version_str != EXPECTED_CLANG_VERSION:
         raise ValueError(
             f"Error: Found clang-format version {version_str}, but {EXPECTED_CLANG_VERSION} is required."
