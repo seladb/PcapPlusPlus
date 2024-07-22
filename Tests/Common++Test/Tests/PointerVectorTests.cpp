aa92f72f4b251fdc0a24cfab359593eb70fae379
Dimitar Krastev <dimi1010100@gmail.com>
2024-07-22 19:06:03 +0300

    Added unit tests for PointerVector and test fixture with built-in memleak detection.

---
+++ Tests/Common++Test/Tests/PointerVectorTests.cpp
@@ -0,0 +1,118 @@
+#include <array>
+#include <algorithm>
+#include <memory>
+#include <cstring>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "..\Utils\MemoryLeakDetectorFixture.hpp"
+
+#include "PointerVector.h"
+
+namespace pcpp
+{
+	class PointerVectorTest : public MemoryLeakDetectorTest
+	{
+	};
+
+	TEST_F(PointerVectorTest, PointerVectorBasics)
+	{
+		PointerVector<int> pVector;
+		PointerVector<int> const& cpVector = pVector;
+		EXPECT_EQ(pVector.size(), 0);
+
+		pVector.pushBack(new int(1));
+		EXPECT_EQ(pVector.size(), 1);
+
+		EXPECT_THROW(pVector.pushBack(nullptr), std::invalid_argument);
+		EXPECT_THROW(pVector.pushBack(std::unique_ptr<int>()), std::invalid_argument);
+
+		int* atIdx0 = pVector.at(0);
+		ASSERT_NE(atIdx0, nullptr);
+		EXPECT_EQ(*atIdx0, 1);
+
+		int const* cAtIdx0 = cpVector.at(0);
+		ASSERT_NE(atIdx0, nullptr);
+		EXPECT_EQ(*atIdx0, 1);
+
+		pVector.pushBack(std::unique_ptr<int>(new int(2)));
+		EXPECT_EQ(pVector.size(), 2);
+
+		{
+			int* atFront = pVector.front();
+			ASSERT_NE(atFront, nullptr);
+			EXPECT_EQ(*atFront, 1);
+
+			int const* cAtFront = cpVector.front();
+			ASSERT_NE(cAtFront, nullptr);
+			EXPECT_EQ(*cAtFront, 1);
+
+			int* atBack = pVector.back();
+			ASSERT_NE(atBack, nullptr);
+			EXPECT_EQ(*atBack, 2);
+
+			int const* cAtBack = cpVector.back();
+			ASSERT_NE(cAtBack, nullptr);
+			EXPECT_EQ(*cAtBack, 2);
+		}
+
+		{
+			auto itBegin = pVector.begin();
+			auto itEnd = pVector.end();
+
+			EXPECT_EQ(std::distance(itBegin, itEnd), 2);
+			EXPECT_EQ(**itBegin, 1);
+			EXPECT_EQ(**std::next(itBegin), 2);
+		}
+
+		{
+			std::unique_ptr<int> p = pVector.getAndDetach(1);
+			EXPECT_EQ(*p, 2);
+			EXPECT_EQ(pVector.size(), 1);
+		}
+
+		PointerVector<int> pVectorCopy = pVector;
+		EXPECT_EQ(pVectorCopy.size(), pVector.size());
+		EXPECT_NE(pVectorCopy.at(0), pVector.at(0));
+		EXPECT_EQ(*pVectorCopy.at(0), *pVector.at(0));
+
+		{
+			PointerVector<int> pVectorMove = std::move(pVector);
+			EXPECT_EQ(pVector.size(), 0);
+			EXPECT_EQ(pVectorMove.size(), 1);
+			EXPECT_EQ(*pVectorMove.at(0), 1);
+
+			pVector = std::move(pVectorMove);
+			ASSERT_EQ(pVector.size(), 1);
+		}
+
+		pVectorCopy.clear();
+		EXPECT_EQ(pVectorCopy.size(), 0);
+		EXPECT_EQ(pVector.size(), 1);
+
+		pVector.pushBack(new int(3));
+		EXPECT_EQ(pVector.size(), 2);
+
+		{
+			int* removed = pVector.getAndRemoveFromVector(pVector.begin());
+			EXPECT_EQ(*removed, 1);
+			EXPECT_EQ(pVector.size(), 1);
+			EXPECT_EQ(*pVector.front(), 3);
+			delete removed;
+		}
+
+		pVector.erase(pVector.begin());
+		EXPECT_EQ(pVector.size(), 0);
+
+		pVector.pushBack(new int(4));
+		pVector.pushBack(new int(5));
+		EXPECT_EQ(pVector.size(), 2);
+
+		{
+			std::unique_ptr<int> p = pVector.getAndDetach(pVector.begin());
+			ASSERT_NE(p, nullptr);
+			EXPECT_EQ(*p, 4);
+			EXPECT_EQ(pVector.size(), 1);
+		}
+	}
+}  // namespace pcpp
---
+++ Tests/Common++Test/Utils/MemoryLeakDetectorFixture.hpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include <memplumber.h>
+
+namespace pcpp
+{
+	class MemoryLeakDetectorTest : public ::testing::Test
+	{
+	protected:
+		void SetUp() override
+		{
+			MemPlumber::start();
+		}
+
+		void TearDown() override
+		{
+			std::size_t memLeakCount = 0;
+			std::uint64_t memLeakSize = 0;
+			MemPlumber::memLeakCheck(memLeakCount, memLeakSize);
+			MemPlumber::stopAndFreeAllMemory();
+
+			if (memLeakCount > 0 || memLeakSize > 0)
+			{
+				FAIL() << "Memory leak found! " << memLeakCount << " objects and " << memLeakSize << " [bytes] leaked";
+			}
+		}
+	};
+	
+}  // namespace pcpp
