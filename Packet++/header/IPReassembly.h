3420d01942cb3b810d079ed9119bfb16d19c1ba7
tigercosmos <phy.tiger@gmail.com>
2024-07-26 04:01:25 +0900

    fix more comment

--- Packet++/header/IPReassembly.h
+++ Packet++/header/IPReassembly.h
@@ -436,13 +436,13 @@ namespace pcpp
 
 		/**
 		 * The main API that drives IPReassembly. This method should be called whenever a fragment arrives. This method
-		 *finds the relevant packet this fragment belongs to and runs the IPv4 reassembly logic that is described in
-		 *IPReassembly.h.
+		 * finds the relevant packet this fragment belongs to and runs the IPv4 reassembly logic that is described in
+		 * IPReassembly.h.
 		 * @param[in] fragment The fragment to process (IPv4 or IPv6). Please notice that the reassembly logic doesn't
-		 *change or manipulate this object in any way. All of its data is copied to internal structures and manipulated
-		 *there
+		 * change or manipulate this object in any way. All of its data is copied to internal structures and manipulated
+		 * there
 		 * @param[out] status An indication of the packet reassembly status following the processing of this fragment.
-		 *Possible values are:
+		 * Possible values are:
 		 * - The input fragment is not a IPv4 or IPv6 packet
 		 * - The input fragment is not a IPv4 or IPv6 fragment packet
 		 * - The input fragment is the first fragment of the packet
@@ -453,22 +453,22 @@ namespace pcpp
 		 * - The input fragment is the last one and the packet is now fully reassembled. In this case the return value
 		 *   will contain a pointer to the reassembled packet
 		 * @param[in] parseUntil Optional parameter. Parse the raw and reassembled packets until you reach a certain
-		 *protocol (inclusive). Can be useful for cases when you need to parse only up to a certain layer and want to
-		 *avoid the performance impact and memory consumption of parsing the whole packet. Note that setting this to a
-		 *protocol which doesn't include the IP-Layer will result in IPReassembly not finding the IP-Layer and thus
-		 *failing to work properly. Default value is ::UnknownProtocol which means don't take this parameter into
-		 *account
+		 * protocol (inclusive). Can be useful for cases when you need to parse only up to a certain layer and want to
+		 * avoid the performance impact and memory consumption of parsing the whole packet. Note that setting this to a
+		 * protocol which doesn't include the IP-Layer will result in IPReassembly not finding the IP-Layer and thus
+		 * failing to work properly. Default value is ::UnknownProtocol which means don't take this parameter into
+		 * account
 		 * @param[in] parseUntilLayer Optional parameter. Parse the raw and reassembled packets until you reach a
-		 *certain layer in the OSI model (inclusive). Can be useful for cases when you need to parse only up to a
-		 *certain OSI layer (for example transport layer) and want to avoid the performance impact and memory
-		 *consumption of parsing the whole packet. Note that setting this value to OsiModelPhysicalLayer will result in
-		 *IPReassembly not finding the IP-layer and thus failing to work properly. Default value is ::UnknownProtocol
-		 *which means don't take this parameter into account Default value is ::OsiModelLayerUnknown which means don't
-		 *take this parameter into account
+		 * certain layer in the OSI model (inclusive). Can be useful for cases when you need to parse only up to a
+		 * certain OSI layer (for example transport layer) and want to avoid the performance impact and memory
+		 * consumption of parsing the whole packet. Note that setting this value to OsiModelPhysicalLayer will result in
+		 * IPReassembly not finding the IP-layer and thus failing to work properly. Default value is ::UnknownProtocol
+		 * which means don't take this parameter into account Default value is ::OsiModelLayerUnknown which means don't
+		 * take this parameter into account
 		 * @return
 		 * - If the input fragment isn't an IPv4/IPv6 packet or if it isn't an IPv4/IPv6 fragment, the return value is a
-		 *   pointer to a Packet object wrapping the input fragment RawPacket object. It's the user responsibility to free
-		 *   this instance
+		 *   pointer to a Packet object wrapping the input fragment RawPacket object. It's the user responsibility to
+		 *   free this instance
 		 * - If the input fragment is the last one and the reassembled packet is ready - a pointer to the reassembled
 		 *   packet is returned. Notice it's the user's responsibility to free this pointer when done using it
 		 * - If the reassembled packet isn't ready then NULL is returned
