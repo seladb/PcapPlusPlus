c10cd6391aac629c6e57cfbb22f141bb2b9053bd
Dimitar Krastev <dimi1010100@gmail.com>
2024-09-01 12:28:41 +0300

    TelnetLayer - Overrides.

--- Packet++/header/TelnetLayer.h
+++ Packet++/header/TelnetLayer.h
@@ -332,19 +332,19 @@ namespace pcpp
 		// overridden methods
 
 		/// Parses the next layer. Telnet is the always last so does nothing for this layer
-		void parseNextLayer()
+		void parseNextLayer() override
 		{}
 
 		/**
 		 * @return Get the size of the layer
 		 */
-		size_t getHeaderLen() const
+		size_t getHeaderLen() const override
 		{
 			return m_DataLen;
 		}
 
 		/// Does nothing for this layer
-		void computeCalculateFields()
+		void computeCalculateFields() override
 		{}
 
 		/**
@@ -358,7 +358,7 @@ namespace pcpp
 		/**
 		 * @return Returns the protocol info as readable string
 		 */
-		std::string toString() const;
+		std::string toString() const override;
 	};
 
 }  // namespace pcpp
