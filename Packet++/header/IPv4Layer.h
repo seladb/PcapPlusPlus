0600167ebba22698633def721e949a078aef98e1
Dimitar Krastev <dimi1010100@gmail.com>
2024-09-01 13:41:35 +0300

    IPv4Layer - Fixed missed overrides.

--- Packet++/header/IPv4Layer.h
+++ Packet++/header/IPv4Layer.h
@@ -346,7 +346,7 @@ namespace pcpp
 
 		// implement abstract methods
 
-		size_t getTotalSize() const
+		size_t getTotalSize() const override
 		{
 			if (m_Data == nullptr)
 				return 0;
@@ -358,7 +358,7 @@ namespace pcpp
 			return static_cast<size_t>(m_Data->recordLen);
 		}
 
-		size_t getDataSize() const
+		size_t getDataSize() const override
 		{
 			if (m_Data == nullptr)
 				return 0;
