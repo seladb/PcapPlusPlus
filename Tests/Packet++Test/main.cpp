bf2a4203cef20e03b228935f341b79c1dfdbb4c4
rndx21033 <90053840+rndx21033@users.noreply.github.com>
2024-09-21 13:37:28 -0600

    Fix crash with parseuntil when top layer is more than the chosen parseuntil OSI layer. (#1580)
    * Fix crash with parseuntil when top layer is more than the chosen parseuntil OSI layer.
    
    * fix format

--- Tests/Packet++Test/main.cpp
+++ Tests/Packet++Test/main.cpp
@@ -161,6 +161,7 @@ int main(int argc, char* argv[])
 	PTF_RUN_TEST(ResizeLayerTest, "packet;resize");
 	PTF_RUN_TEST(PrintPacketAndLayersTest, "packet;print");
 	PTF_RUN_TEST(ProtocolFamilyMembershipTest, "packet");
+	PTF_RUN_TEST(PacketParseLayerLimitTest, "packet");
 
 	PTF_RUN_TEST(HttpRequestParseMethodTest, "http");
 	PTF_RUN_TEST(HttpRequestLayerParsingTest, "http");
