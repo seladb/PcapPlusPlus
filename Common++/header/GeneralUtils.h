f81ced259b2aea3cfc4a779c4ec1678a0aa1a0b7
Dimitar Krastev <dimi1010100@gmail.com>
2024-12-10 18:11:30 +0200

    Reformat Common++ documentation to use triple slash. (#1647)
    * Reformatted Common++ to use triple slash /// instead of /** for documentation.
    
    * Fixed potentially undefined `size_t`.
    
    * Fixed string include.
    
    * Reverted std::size_t to size_t.

--- Common++/header/GeneralUtils.h
+++ Common++/header/GeneralUtils.h
@@ -6,56 +6,46 @@
 
 /// @file
 
-/**
- * \namespace pcpp
- * \brief The main namespace for the PcapPlusPlus lib
- */
+/// @namespace pcpp
+/// @brief The main namespace for the PcapPlusPlus lib
 namespace pcpp
 {
-	/**
-	 * Convert a byte array into a string of hex characters. For example: for the array { 0xaa, 0x2b, 0x10 } the string
-	 * "aa2b10" will be returned
-	 * @param[in] byteArr A byte array
-	 * @param[in] byteArrSize The size of the byte array [in bytes]
-	 * @param[in] stringSizeLimit An optional parameter that enables to limit the returned string size. If set to a
-	 * positive integer value the returned string size will be equal or less than this value. If the string
-	 * representation of the whole array is longer than this size then only part of the array will be read. The default
-	 * value is -1 which means no string size limitation
-	 * @return A string of hex characters representing the byte array
-	 */
+	/// Convert a byte array into a string of hex characters. For example: for the array { 0xaa, 0x2b, 0x10 } the string
+	/// "aa2b10" will be returned
+	/// @param[in] byteArr A byte array
+	/// @param[in] byteArrSize The size of the byte array [in bytes]
+	/// @param[in] stringSizeLimit An optional parameter that enables to limit the returned string size. If set to a
+	/// positive integer value the returned string size will be equal or less than this value. If the string
+	/// representation of the whole array is longer than this size then only part of the array will be read. The default
+	/// value is -1 which means no string size limitation
+	/// @return A string of hex characters representing the byte array
 	std::string byteArrayToHexString(const uint8_t* byteArr, size_t byteArrSize, int stringSizeLimit = -1);
 
-	/**
-	 * Convert a string of hex characters into a byte array. For example: for the string "aa2b10" an array of values
-	 * { 0xaa, 0x2b, 0x10 } will be returned
-	 * @param[in] hexString A string of hex characters
-	 * @param[out] resultByteArr A pre-allocated byte array where the result will be written to
-	 * @param[in] resultByteArrSize The size of the pre-allocated byte array
-	 * @return The size of the result array. If the string represents an array that is longer than the pre-allocated
-	 * size (resultByteArrSize) then the result array will contain only the part of the string that managed to fit into
-	 * the array, and the returned size will be resultByteArrSize. However if the string represents an array that is
-	 * shorter than the pre-allocated size then some of the cells will remain empty and contain zeros, and the returned
-	 * size will be the part of the array that contain data. If the input is an illegal hex string 0 will be returned.
-	 * Illegal hex string means odd number of characters or a string that contains non-hex characters
-	 */
+	/// Convert a string of hex characters into a byte array. For example: for the string "aa2b10" an array of values
+	/// { 0xaa, 0x2b, 0x10 } will be returned
+	/// @param[in] hexString A string of hex characters
+	/// @param[out] resultByteArr A pre-allocated byte array where the result will be written to
+	/// @param[in] resultByteArrSize The size of the pre-allocated byte array
+	/// @return The size of the result array. If the string represents an array that is longer than the pre-allocated
+	/// size (resultByteArrSize) then the result array will contain only the part of the string that managed to fit into
+	/// the array, and the returned size will be resultByteArrSize. However if the string represents an array that is
+	/// shorter than the pre-allocated size then some of the cells will remain empty and contain zeros, and the returned
+	/// size will be the part of the array that contain data. If the input is an illegal hex string 0 will be returned.
+	/// Illegal hex string means odd number of characters or a string that contains non-hex characters
 	size_t hexStringToByteArray(const std::string& hexString, uint8_t* resultByteArr, size_t resultByteArrSize);
 
-	/**
-	 * This is a cross platform version of memmem (https://man7.org/linux/man-pages/man3/memmem.3.html) which is not
-	 * supported on all platforms.
-	 * @param[in] haystack A pointer to the buffer to be searched
-	 * @param[in] haystackLen Length of the haystack buffer
-	 * @param[in] needle A pointer to a buffer that will be searched for
-	 * @param[in] needleLen Length of the needle buffer
-	 * @return A pointer to the beginning of the substring, or nullptr if the substring is not found
-	 */
+	/// This is a cross platform version of memmem (https://man7.org/linux/man-pages/man3/memmem.3.html) which is not
+	/// supported on all platforms.
+	/// @param[in] haystack A pointer to the buffer to be searched
+	/// @param[in] haystackLen Length of the haystack buffer
+	/// @param[in] needle A pointer to a buffer that will be searched for
+	/// @param[in] needleLen Length of the needle buffer
+	/// @return A pointer to the beginning of the substring, or nullptr if the substring is not found
 	char* cross_platform_memmem(const char* haystack, size_t haystackLen, const char* needle, size_t needleLen);
 
-	/**
-	 * Calculates alignment.
-	 * @param[in] number Given number
-	 * @return The aligned number
-	 */
+	/// Calculates alignment.
+	/// @param[in] number Given number
+	/// @return The aligned number
 	template <int alignment> static int align(int number)
 	{
 		// Only works for alignment with power of 2
@@ -65,10 +55,8 @@ namespace pcpp
 		return (number + mask) & ~mask;
 	}
 
-	/**
-	 * A template class to calculate enum class hash
-	 * @tparam EnumClass
-	 */
+	/// A template class to calculate enum class hash
+	/// @tparam EnumClass
 	template <typename EnumClass, typename std::enable_if<std::is_enum<EnumClass>::value, bool>::type = false>
 	struct EnumClassHash
 	{
--- Common++/header/IpUtils.h
+++ Common++/header/IpUtils.h
@@ -25,83 +25,66 @@
 // We use "__MINGW64_VERSION_MAJOR" and not __MINGW64__ to detect MinGW-w64 compiler
 // because the second one is not defined for MinGW-w64 in 32bits mode
 #if defined(_WIN32) && !defined(_MSC_VER) && (!defined(__MINGW64_VERSION_MAJOR) || (__MINGW64_VERSION_MAJOR < 8))
-/**
- * Convert a network format address to presentation format.
- * @param[in] af Address family, can be either AF_INET (IPv4) or AF_INET6 (IPv6)
- * @param[in] src Network address structure, can be either in_addr (IPv4) or in6_addr (IPv6)
- * @param[out] dst Network address string representation
- * @param[in] size 'dst' Maximum size
- * @return pointer to presentation format address ('dst'), or nullptr (see errno).
- */
+
+/// Convert a network format address to presentation format.
+/// @param[in] af Address family, can be either AF_INET (IPv4) or AF_INET6 (IPv6)
+/// @param[in] src Network address structure, can be either in_addr (IPv4) or in6_addr (IPv6)
+/// @param[out] dst Network address string representation
+/// @param[in] size 'dst' Maximum size
+/// @return pointer to presentation format address ('dst'), or nullptr (see errno).
 const char* inet_ntop(int af, const void* src, char* dst, size_t size);
 
-/**
- * Convert from presentation format (which usually means ASCII printable)
- * to network format (which is usually some kind of binary format).
- * @param[in] af Address family, can be either AF_INET (IPv4) or AF_INET6 (IPv6)
- * @param[in] src Network address string representation
- * @param[out] dst Network address structure result, can be either in_addr (IPv4) or in6_addr (IPv6)
- * @return
- * 1 if the address was valid for the specified address family;
- * 0 if the address wasn't valid ('dst' is untouched in this case);
- * -1 if some other error occurred ('dst' is untouched in this case, too)
- */
+/// Convert from presentation format (which usually means ASCII printable)
+/// to network format (which is usually some kind of binary format).
+/// @param[in] af Address family, can be either AF_INET (IPv4) or AF_INET6 (IPv6)
+/// @param[in] src Network address string representation
+/// @param[out] dst Network address structure result, can be either in_addr (IPv4) or in6_addr (IPv6)
+/// @return
+/// 1 if the address was valid for the specified address family;
+/// 0 if the address wasn't valid ('dst' is untouched in this case);
+/// -1 if some other error occurred ('dst' is untouched in this case, too)
 int inet_pton(int af, const char* src, void* dst);
 #endif
 
-/**
- * \namespace pcpp
- * \brief The main namespace for the PcapPlusPlus lib
- */
+/// @namespace pcpp
+/// @brief The main namespace for the PcapPlusPlus lib
 namespace pcpp
 {
 	namespace internal
 	{
-		/**
-		 * Extract IPv4 address from sockaddr
-		 * @param[in] sa - input sockaddr
-		 * @return Address in in_addr format
-		 * @throws std::invalid_argument Sockaddr family is not AF_INET or sockaddr is nullptr.
-		 */
+		/// Extract IPv4 address from sockaddr
+		/// @param[in] sa - input sockaddr
+		/// @return Address in in_addr format
+		/// @throws std::invalid_argument Sockaddr family is not AF_INET or sockaddr is nullptr.
 		in_addr* sockaddr2in_addr(sockaddr* sa);
 
-		/**
-		 * Attempt to extract IPv4 address from sockaddr
-		 * @param[in] sa - input sockaddr
-		 * @return Pointer to address in in_addr format or nullptr if extraction fails.
-		 */
+		/// Attempt to extract IPv4 address from sockaddr
+		/// @param[in] sa - input sockaddr
+		/// @return Pointer to address in in_addr format or nullptr if extraction fails.
 		in_addr* try_sockaddr2in_addr(sockaddr* sa);
 
-		/**
-		 * Extract IPv6 address from sockaddr
-		 * @param[in] sa - input sockaddr
-		 * @return Address in in6_addr format
-		 * @throws std::invalid_argument Sockaddr family is not AF_INET6 or sockaddr is nullptr.
-		 */
+		/// Extract IPv6 address from sockaddr
+		/// @param[in] sa - input sockaddr
+		/// @return Address in in6_addr format
+		/// @throws std::invalid_argument Sockaddr family is not AF_INET6 or sockaddr is nullptr.
 		in6_addr* sockaddr2in6_addr(sockaddr* sa);
 
-		/**
-		 * Attempt to extract IPv6 address from sockaddr
-		 * @param[in] sa - input sockaddr
-		 * @return Pointer to address in in6_addr format or nullptr if extraction fails.
-		 */
+		/// Attempt to extract IPv6 address from sockaddr
+		/// @param[in] sa - input sockaddr
+		/// @return Pointer to address in in6_addr format or nullptr if extraction fails.
 		in6_addr* try_sockaddr2in6_addr(sockaddr* sa);
 
-		/**
-		 * Converts a sockaddr format address to its string representation
-		 * @param[in] sa Address in sockaddr format
-		 * @param[out] resultString String representation of the address
-		 * @param[in] resultBufLen Length of the result buffer.
-		 * @throws std::invalid_argument Sockaddr family is not AF_INET or AF_INET6, sockaddr is nullptr or the result
-		 * str buffer is insufficient.
-		 */
+		/// Converts a sockaddr format address to its string representation
+		/// @param[in] sa Address in sockaddr format
+		/// @param[out] resultString String representation of the address
+		/// @param[in] resultBufLen Length of the result buffer.
+		/// @throws std::invalid_argument Sockaddr family is not AF_INET or AF_INET6, sockaddr is nullptr or the result
+		/// str buffer is insufficient.
 		void sockaddr2string(sockaddr const* sa, char* resultString, size_t resultBufLen);
 
-		/**
-		 * Convert a in_addr format address to 32bit representation
-		 * @param[in] inAddr Address in in_addr format
-		 * @return Address in 32bit format
-		 */
+		/// Convert a in_addr format address to 32bit representation
+		/// @param[in] inAddr Address in in_addr format
+		/// @return Address in 32bit format
 		uint32_t in_addr2int(in_addr inAddr);
 	}  // namespace internal
 }  // namespace pcpp
