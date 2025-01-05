0ac8f928caadb564dac8a37ef4cf633696b73034
Dimitar Krastev <dimi1010100@gmail.com>
2025-01-05 11:14:15 +0200

    Changed `IPNetwork` copy assignment implementation to copy-and-swap the internal network.
    This change fixes use of memory after being freed in case of self-assignment and provides a strong exception guarantee.

--- Common++/header/IpAddress.h
+++ Common++/header/IpAddress.h
@@ -707,18 +707,9 @@ namespace pcpp
 		/// @return A reference to the assignee
 		IPNetwork& operator=(const IPv4Network& other)
 		{
-			if (m_IPv4Network)
-			{
-				m_IPv4Network = nullptr;
-			}
-
-			if (m_IPv6Network)
-			{
-				m_IPv6Network = nullptr;
-			}
-
-			m_IPv4Network = std::unique_ptr<IPv4Network>(new IPv4Network(other));
-
+			auto newNetwork = std::unique_ptr<IPv4Network>(new IPv4Network(other));
+			m_IPv4Network = std::move(newNetwork);
+			m_IPv6Network = nullptr;
 			return *this;
 		}
 
@@ -727,18 +718,9 @@ namespace pcpp
 		/// @return A reference to the assignee
 		IPNetwork& operator=(const IPv6Network& other)
 		{
-			if (m_IPv4Network)
-			{
-				m_IPv4Network = nullptr;
-			}
-
-			if (m_IPv6Network)
-			{
-				m_IPv6Network = nullptr;
-			}
-
-			m_IPv6Network = std::unique_ptr<IPv6Network>(new IPv6Network(other));
-
+			auto newNetwork = std::unique_ptr<IPv6Network>(new IPv6Network(other));
+			m_IPv4Network = nullptr;
+			m_IPv6Network = std::move(newNetwork);
 			return *this;
 		}
 
