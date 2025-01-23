e5cf5fb0cb63e2ef1a5f7a6232eee3e89208efa9
Dimitar Krastev <dimi1010100@gmail.com>
2025-01-23 11:05:39 +0200

    Added build information to the test executable.

--- Tests/Common++Test/main.cpp
+++ Tests/Common++Test/main.cpp
@@ -1,8 +1,16 @@
+#include <iostream>
+
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+#include "PcapPlusPlusVersion.h"
+
 int main(int argc, char* argv[])
 {
+	std::cout << "PcapPlusPlus version: " << pcpp::getPcapPlusPlusVersionFull() << '\n'
+	          << "Built: " << pcpp::getBuildDateTime() << '\n'
+	          << "Built from: " << pcpp::getGitInfo() << std::endl;
+
 	::testing::InitGoogleMock(&argc, argv);
 	return RUN_ALL_TESTS();
 }
