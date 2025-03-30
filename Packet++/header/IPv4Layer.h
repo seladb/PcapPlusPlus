72baa0faae9d233cc198cc100b3efe3346756427
Dimitar Krastev <dimi1010100@gmail.com>
2025-03-31 01:40:08 +0300

    Lint

--- Packet++/header/IPv4Layer.h
+++ Packet++/header/IPv4Layer.h
@@ -626,7 +626,8 @@ namespace pcpp
 	bool IPv4Layer::isDataValid(const uint8_t* data, size_t dataLen)
 	{
 		const iphdr* hdr = reinterpret_cast<const iphdr*>(data);
-		return internal::canReinterpretAs<iphdr>(data, dataLen) && hdr->ipVersion == 4 && hdr->internetHeaderLength >= 5;
+		return internal::canReinterpretAs<iphdr>(data, dataLen) && hdr->ipVersion == 4 &&
+		       hdr->internetHeaderLength >= 5;
 	}
 
 }  // namespace pcpp
