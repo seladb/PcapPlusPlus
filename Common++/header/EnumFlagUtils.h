629218aa6ae7ff048715b6805a41470ef5ffa78c
Dimitar Krastev <dimi1010100@gmail.com>
2025-05-18 12:12:02 +0300

    Added utilities for declaring enum class flags and allowing bitwise operations on them.

---
+++ Common++/header/EnumFlagUtils.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <type_traits>
+
+namespace pcpp
+{
+	namespace internal
+	{
+		template <typename EnumClass> struct EnableBitMaskOperators : std::false_type
+		{
+		};
+
+		template <typename EnumClass>
+		using EnableIfBitMask = typename std::enable_if<EnableBitMaskOperators<EnumClass>::value, EnumClass>::type;
+
+		template <typename EnumClass> constexpr EnableIfBitMask<EnumClass> operator|(EnumClass lhs, EnumClass rhs)
+		{
+			return static_cast<EnumClass>(static_cast<std::underlying_type<EnumClass>::type>(lhs) |
+			                              static_cast<std::underlying_type<EnumClass>::type>(rhs));
+		}
+
+		template <typename EnumClass> constexpr EnableIfBitMask<EnumClass> operator&(EnumClass lhs, EnumClass rhs)
+		{
+			return static_cast<EnumClass>(static_cast<std::underlying_type<EnumClass>::type>(lhs) &
+			                              static_cast<std::underlying_type<EnumClass>::type>(rhs));
+		}
+
+		template <typename EnumClass> constexpr EnableIfBitMask<EnumClass> operator^(EnumClass lhs, EnumClass rhs)
+		{
+			return static_cast<EnumClass>(static_cast<std::underlying_type<EnumClass>::type>(lhs) ^
+			                              static_cast<std::underlying_type<EnumClass>::type>(rhs));
+		}
+
+		template <typename EnumClass> constexpr EnableIfBitMask<EnumClass> operator~(EnumClass rhs)
+		{
+			return static_cast<EnumClass>(~static_cast<std::underlying_type<EnumClass>::type>(rhs));
+		}
+
+		template <typename EnumClass> constexpr EnableIfBitMask<EnumClass>& operator|=(EnumClass& lhs, EnumClass rhs)
+		{
+			return lhs = lhs | rhs;
+		}
+
+		template <typename EnumClass> constexpr EnableIfBitMask<EnumClass>& operator&=(EnumClass& lhs, EnumClass rhs)
+		{
+			return lhs = lhs & rhs;
+		}
+
+		template <typename EnumClass> constexpr EnableIfBitMask<EnumClass>& operator^=(EnumClass& lhs, EnumClass rhs)
+		{
+			return lhs = lhs ^ rhs;
+		}
+
+		template <typename EnumClass> constexpr bool hasFlag(EnableIfBitMask<EnumClass> value, EnumClass flag)
+		{
+			return (value & flag) == flag;
+		}
+	}  // namespace internal
+}  // namespace pcpp
+
+#define PCPP_DECLARE_ENUM_FLAG(EnumClass)                                                                              \
+	template <> struct ::pcpp::internal::EnableBitMaskOperators<EnumClass> : std::true_type                            \
+	{                                                                                                                  \
+	};
+
+#define PCPP_USING_ENUM_FLAG_OPERATORS()                                                                               \
+	using ::pcpp::internal::operator|;                                                                                 \
+	using ::pcpp::internal::operator&;                                                                                 \
+	using ::pcpp::internal::operator^;                                                                                 \
+	using ::pcpp::internal::operator~;                                                                                 \
+	using ::pcpp::internal::operator|=;                                                                                \
+	using ::pcpp::internal::operator&=;                                                                                \
+	using ::pcpp::internal::operator^=;                                                                                \
+	using ::pcpp::internal::hasFlag;
