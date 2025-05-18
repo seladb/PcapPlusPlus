a36e5945714204cfd3f5336bd51de5ba40571004
yahyayozo <yahya.aouledamer@gmail.com>
2025-05-18 19:09:33 +0100

    Add ModbusLayer first implementation with header parsing only

---
+++ Packet++/header/ModbusLayer.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "Layer.h"
+
+/// @file
+/// This file contains classes for parsing, creating and editing Modbus packets.
+
+/// @namespace pcpp
+/// @brief The main namespace for the PcapPlusPlus lib
+namespace pcpp
+{
+
+#pragma pack(push, 1)
+	/// @struct modbus_common_header
+	/// MODBUS Application Protocol header
+	struct modbus_common_header
+	{
+		/// For synchronization between messages of server and client
+		uint16_t transactionId;
+		/// 0 for Modbus/TCP
+		uint16_t protocolId;
+		/// Number of remaining bytes in this frame starting from the unit id
+		uint16_t length;
+		/// Unit identifier
+		uint8_t unitId;
+		/// Function code
+		uint8_t functionCode;
+	};
+#pragma pack(pop)
+
+	class ModbusLayer : public Layer
+	{
+	public:
+		static_assert(sizeof(modbus_common_header) == 8, "modbus_common_header size is not 8 bytes");
+
+		/// A constructor that creates the layer with empty fields
+		ModbusLayer();
+
+		/// @return A pointer to the MODBUS header
+		modbus_common_header* getModbusHeader() const;
+
+		/// @return MODBUS message type
+		uint16_t getTransactionId();
+
+		/// @return MODBUS protocol id
+		uint16_t getProtocolId();
+
+		/// @return MODBUS remaining bytes in frame starting from the unit id
+		/// @note This is the length of the MODBUS payload + unit_id, not the entire packet
+		uint16_t getLength();
+
+		/// @return MODBUS unit id
+		uint8_t getUnitId();
+
+		/// @return MODBUS function code
+		uint8_t getFunctionCode();
+	}
+
+}  // namespace pcpp
---
+++ Packet++/src/ModbusLayer.cpp
@@ -0,0 +1,52 @@
+
+#include "ModbusLayer.h"
+
+namespace pcpp
+{
+	ModbusLayer::ModbusLayer()
+	{
+		const size_t headerLen = sizeof(modbus_common_header);
+		m_DataLen = headerLen;
+		m_Data = new uint8_t[headerLen];
+		memset(m_Data, 0, headerLen);
+
+		// Initialize the header fields to default values
+		modbus_common_header* header = getModbusHeader();
+		header->transactionId = 0;
+		header->protocolId = 0;  // 0 for Modbus/TCP
+		header->length = 2;      // minimum length of the MODBUS payload + unit_id
+		header->unitId = 0;
+		header->functionCode = 0;
+	}
+
+	modbus_common_header* ModbusLayer::getModbusHeader() const
+	{
+		return (modbus_common_header*)m_Data;
+	}
+
+	uint16_t ModbusLayer::getTransactionId()
+	{
+		return getModbusHeader()->transactionId;
+	}
+
+	uint16_t ModbusLayer::getProtocolId()
+	{
+		return getModbusHeader()->protocolId;
+	}
+
+	uint16_t ModbusLayer::getLength()
+	{
+		return getModbusHeader()->length;
+	}
+
+	uint8_t ModbusLayer::getUnitId()
+	{
+		return getModbusHeader()->unitId;
+	}
+
+	uint8_t ModbusLayer::getFunctionCode()
+	{
+		return getModbusHeader()->functionCode;
+	}
+
+}  // namespace pcpp
