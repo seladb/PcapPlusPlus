9f4ebbd5fffdb862561a4c1833ef5b4f93079bb3
yahyayozo <yahya.aouledamer@gmail.com>
2025-06-04 22:36:50 +0100

    Refactor ModbusLayerParsingTest to create ModbusLayer from a dat file instead of raw data

--- Tests/Packet++Test/Tests/ModbusTests.cpp
+++ Tests/Packet++Test/Tests/ModbusTests.cpp
@@ -1,5 +1,6 @@
 #include "../TestDefinition.h"
 #include "../Utils/TestUtils.h"
+#include "Packet.h"
 #include "ModbusLayer.h"
 #include "EndianPortable.h"
 #include "SystemUtils.h"
@@ -22,14 +23,20 @@ PTF_TEST_CASE(ModbusLayerCreationTest)
 
 PTF_TEST_CASE(ModbusLayerParsingTest)
 {
-	// Create a ModbusLayer from raw data
-	uint8_t rawData[] = { 0x30, 0x39, 0x00, 0x00, 0x00, 0x02, 0x01, 0x03 };  // Example raw data
-	pcpp::ModbusLayer modbusLayer(rawData, sizeof(rawData), nullptr, nullptr);
+	// Create a ModbusLayer from dat file
+	timeval time;
+	gettimeofday(&time, nullptr);
 
-	PTF_ASSERT_EQUAL(modbusLayer.getTransactionId(), 12345);
-	PTF_ASSERT_EQUAL(modbusLayer.getProtocolId(), 0);
-	PTF_ASSERT_EQUAL(modbusLayer.getLength(), 2);  // minimum length of the MODBUS payload + unit_id
-	PTF_ASSERT_EQUAL(modbusLayer.getUnitId(), 1);
-	PTF_ASSERT_EQUAL(modbusLayer.getFunctionCode(), 3);
-	PTF_ASSERT_EQUAL(modbusLayer.getHeaderLen(), sizeof(pcpp::modbus_common_header));
+	READ_FILE_AND_CREATE_PACKET(1, "PacketExamples/ModbusPacket.dat");
+	pcpp::Packet modbusPacket(&rawPacket1);
+	PTF_ASSERT_TRUE(modbusPacket.isPacketOfType(pcpp::Modbus));
+	pcpp::ModbusLayer* modbusLayer = modbusPacket.getLayerOfType<pcpp::ModbusLayer>();
+
+	PTF_ASSERT_NOT_NULL(modbusLayer);
+	PTF_ASSERT_EQUAL(modbusLayer->getTransactionId(), 12345);
+	PTF_ASSERT_EQUAL(modbusLayer->getProtocolId(), 0);
+	PTF_ASSERT_EQUAL(modbusLayer->getLength(), 2);  // minimum length of the MODBUS payload + unit_id
+	PTF_ASSERT_EQUAL(modbusLayer->getUnitId(), 1);
+	PTF_ASSERT_EQUAL(modbusLayer->getFunctionCode(), 3);
+	PTF_ASSERT_EQUAL(modbusLayer->getHeaderLen(), sizeof(pcpp::modbus_common_header));
 }  // ModbusLayerParsingTest
