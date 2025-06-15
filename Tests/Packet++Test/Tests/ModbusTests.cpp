adb3f518569943a601d5d9f23dfb7ee414ffcb33
yahyayozo <yahya.aouledamer@gmail.com>
2025-06-15 20:26:21 +0100

    Remove redundant ModbusLayerParsingTest case from ModbusTests.cpp

--- Tests/Packet++Test/Tests/ModbusTests.cpp
+++ Tests/Packet++Test/Tests/ModbusTests.cpp
@@ -20,23 +20,3 @@ PTF_TEST_CASE(ModbusLayerCreationTest)
 	                 "Modbus Layer, Transaction ID: 12345, Protocol ID: 0, Length: 2, Unit ID: 1, Function Code: 3");
 	PTF_ASSERT_EQUAL(modbusLayer.getOsiModelLayer(), pcpp::OsiModelApplicationLayer);
 }  // ModbusLayerCreationTest
-
-PTF_TEST_CASE(ModbusLayerParsingTest)
-{
-	// Create a ModbusLayer from dat file
-	timeval time;
-	gettimeofday(&time, nullptr);
-
-	READ_FILE_AND_CREATE_PACKET(1, "PacketExamples/ModbusPacket.dat");
-	pcpp::Packet modbusPacket(&rawPacket1);
-	PTF_ASSERT_TRUE(modbusPacket.isPacketOfType(pcpp::Modbus));
-	pcpp::ModbusLayer* modbusLayer = modbusPacket.getLayerOfType<pcpp::ModbusLayer>();
-
-	PTF_ASSERT_NOT_NULL(modbusLayer);
-	PTF_ASSERT_EQUAL(modbusLayer->getTransactionId(), 12345);
-	PTF_ASSERT_EQUAL(modbusLayer->getProtocolId(), 0);
-	PTF_ASSERT_EQUAL(modbusLayer->getLength(), 2);  // minimum length of the MODBUS payload + unit_id
-	PTF_ASSERT_EQUAL(modbusLayer->getUnitId(), 1);
-	PTF_ASSERT_EQUAL(modbusLayer->getFunctionCode(), 3);
-	PTF_ASSERT_EQUAL(modbusLayer->getHeaderLen(), sizeof(pcpp::modbus_common_header));
-}  // ModbusLayerParsingTest
