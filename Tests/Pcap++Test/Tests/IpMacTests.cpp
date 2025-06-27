4143274758229bf5defd4e8112083791957d518f
Dimitar Krastev <dimi1010100@gmail.com>
2025-06-27 22:40:57 +0300

    Simplified tests.

--- Tests/Pcap++Test/Tests/IpMacTests.cpp
+++ Tests/Pcap++Test/Tests/IpMacTests.cpp
@@ -131,22 +131,15 @@ PTF_TEST_CASE(TestIPAddress)
 	}
 
 	std::array<uint8_t, 16> addrAsByteArray;
-	ip6AddrFromIpAddr.copyTo(addrAsByteArray.data());
 	std::array<uint8_t, 16> expectedByteArray = { 0x26, 0x07, 0xF0, 0xD0, 0x10, 0x02, 0x00, 0x51,
 		                                          0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04 };
-	for (int i = 0; i < 16; i++)
-	{
-		PTF_ASSERT_EQUAL(addrAsByteArray[i], expectedByteArray[i]);
-	}
+	ip6AddrFromIpAddr.copyTo(addrAsByteArray.data());
+	PTF_ASSERT_VECTORS_EQUAL(addrAsByteArray, expectedByteArray);
 
 	// Test copyTo with size
 	addrAsByteArray.fill(0);
 	PTF_ASSERT_EQUAL(ip6AddrFromIpAddr.copyTo(addrAsByteArray.data(), addrAsByteArray.size()), 16);
-
-	for (int i = 0; i < 16; i++)
-	{
-		PTF_ASSERT_EQUAL(addrAsByteArray[i], expectedByteArray[i]);
-	}
+	PTF_ASSERT_VECTORS_EQUAL(addrAsByteArray, expectedByteArray);
 
 	// Test copyTo with size less than 16
 	addrAsByteArray.fill(0);
