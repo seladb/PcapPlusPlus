3803685814d6e6f95f9a84b70d76841fe68705c5
Dimitar Krastev <dimi1010100@gmail.com>
2025-07-12 14:30:11 +0300

    Removed aliases

--- Tests/Pcap++Test/Tests/IpMacTests.cpp
+++ Tests/Pcap++Test/Tests/IpMacTests.cpp
@@ -21,19 +21,15 @@ extern PcapTestArgs PcapTestGlobalArgs;
 
 PTF_TEST_CASE(TestIPAddress)
 {
-	using pcpp::IPAddress;
-	using pcpp::IPv4Address;
-	using pcpp::IPv6Address;
-
 	{
 		std::array<uint8_t, 16> buf;
 
-		PTF_ASSERT_RAISES(IPv4Address(nullptr, 0), std::invalid_argument, "Buffer pointer is null");
-		PTF_ASSERT_RAISES(IPv4Address(buf.data(), 3), std::out_of_range,
+		PTF_ASSERT_RAISES(pcpp::IPv4Address(nullptr, 0), std::invalid_argument, "Buffer pointer is null");
+		PTF_ASSERT_RAISES(pcpp::IPv4Address(buf.data(), 3), std::out_of_range,
 		                  "Buffer size is smaller than IPv4 address size");
 
-		PTF_ASSERT_RAISES(IPv6Address(nullptr, 0), std::invalid_argument, "Buffer pointer is null");
-		PTF_ASSERT_RAISES(IPv6Address(buf.data(), 15), std::out_of_range,
+		PTF_ASSERT_RAISES(pcpp::IPv6Address(nullptr, 0), std::invalid_argument, "Buffer pointer is null");
+		PTF_ASSERT_RAISES(pcpp::IPv6Address(buf.data(), 15), std::out_of_range,
 		                  "Buffer size is smaller than IPv6 address size");
 	}
 
@@ -300,8 +296,6 @@ PTF_TEST_CASE(TestIPAddress)
 
 PTF_TEST_CASE(TestMacAddress)
 {
-	using pcpp::MacAddress;
-
 	pcpp::MacAddress macAddr1(0x11, 0x2, 0x33, 0x4, 0x55, 0x6);
 	pcpp::MacAddress macAddr2(0x11, 0x2, 0x33, 0x4, 0x55, 0x6);
 	PTF_ASSERT_EQUAL(macAddr1, macAddr2);
@@ -318,8 +312,8 @@ PTF_TEST_CASE(TestMacAddress)
 	PTF_ASSERT_RAISES(pcpp::MacAddress{ invalidCharArrayAddress }, std::invalid_argument,
 	                  "Invalid MAC address format, should be xx:xx:xx:xx:xx:xx");
 
-	PTF_ASSERT_RAISES(MacAddress(nullptr, 0), std::invalid_argument, "Address pointer is null");
-	PTF_ASSERT_RAISES(MacAddress(addrAsArr, 4), std::out_of_range,
+	PTF_ASSERT_RAISES(pcpp::MacAddress(nullptr, 0), std::invalid_argument, "Address pointer is null");
+	PTF_ASSERT_RAISES(pcpp::MacAddress(addrAsArr, 4), std::out_of_range,
 	                  "Buffer size is smaller than MAC address size (6 bytes)")
 
 	PTF_ASSERT_EQUAL(macAddr1.toString(), "11:02:33:04:55:06");
