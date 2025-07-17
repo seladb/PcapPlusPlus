938c74ee646f6bc716c5317ff10c2cc0cb386599
Dimitar Krastev <dimi1010100@gmail.com>
2025-07-17 19:43:06 +0300

    Moved some functions to cpp file.

--- Packet++/header/ParserConfig.h
+++ Packet++/header/ParserConfig.h
@@ -26,103 +26,60 @@ namespace pcpp
 		{
 			return { AnyPort, portDst };
 		}
-	};
 
+		constexpr bool operator==(const PortPair& other) const noexcept
+		{
+			return portSrc == other.portSrc && portDst == other.portDst;
+		}
+
+		friend std::ostream& operator<<(std::ostream& os, PortPair const pair)
+		{
+			os << "PortPair(src: " << pair.portSrc << ", dst: " << pair.portDst << ")";
+			return os;
+		}
+	};
+}  // namespace pcpp
+
+namespace std
+{
+	template <>
+	struct hash<pcpp::PortPair>
+	{
+		size_t operator()(const pcpp::PortPair& portPair) const noexcept
+		{
+			return std::hash<uint32_t>()(portPair.portDst << 16 | portPair.portSrc);
+		}
+	};
+}  // namespace std
+
+namespace pcpp
+{
 	class PortMapper
 	{
 	public:
 		/// @brief Create an empty PortMapper.
 		PortMapper() = default;
-		/// @brief Create a PortMapper with a predefined mapping of ports to protocol types.
-		/// @param portToProtocolMap An unordered map where keys are port numbers and values are ProtocolType values.
-		PortMapper(std::unordered_map<PortPair, ProtocolType> portToProtocolMap)
-		    : m_PortToProtocolMap(std::move(portToProtocolMap))
-		{}
 
 		/// @brief Add a port mapping to the port mapper.
 		/// @param port The port number to map.
 		/// @param protocol The ProtocolType to associate with the port.
-		/// @param symmetrical If true, the mapping is considered symmetrical (both src and dst ports are the same).
-		void addPortMapping(PortPair port, ProtocolType protocol, bool symmetrical = false)
-		{
-			if (port == PortPair())
-			{
-				throw std::invalid_argument("PortPair cannot be empty (both src and dst ports are 0)");
-			}
-
-			auto insertResult = m_PortToProtocolMap.insert({ port, protocol });
-			insertResult.first->second = protocol;  // Update the protocol if it already exists
-			if (!insertResult.second)
-			{
-				PCPP_LOG_WARN("Port " << port << " is already mapped to protocol " << insertResult.first->second
-				                      << ", updating to " << protocol);
-			}
-
-			if (symmetrical && port.portSrc != port.portDst)
-			{
-				// Add the symmetrical mapping
-				PortPair symmetricalPort = { port.portDst, port.portSrc };
-				addPortMapping(symmetricalPort, protocol, false);
-			}
-		}
+		/// @param symmetrical If true, the mapping is considered symmetrical (both src and dst ports are the interchangable).
+		void addPortMapping(PortPair port, ProtocolType protocol, bool symmetrical = false);
 
 		/// @brief Remove a port mapping from the port mapper.
 		/// @param port The port number to remove from the mapping.
-		void removePortMapping(PortPair port)
-		{
-			auto it = m_PortToProtocolMap.find(port);
-			if (it != m_PortToProtocolMap.end())
-			{
-				m_PortToProtocolMap.erase(it);
-			}
-			else
-			{
-				PCPP_LOG_DEBUG("Port " << port << " not found in port mapper, nothing to remove");
-			}
-		}
+		/// @param symmetrical If true, the mapping is considered symmetrical (both src and dst ports are the interchangable).
+		void removePortMapping(PortPair port, bool symmetrical = false);
 
 		/// @brief Get the protocol type associated with a specific port.
-		/// 
+		///
 		/// The method checks for an exact match of the port pair first.
 		/// If `exact` is false, it will also check for a match on either the source or destination port.
-		/// 
+		///
 		/// @param port The port number to look up.
 		/// @param exact If true, only an exact match of the port pair is considered. If false, src or dst port matches
 		/// @return The ProtocolType associated with the port, or UnknownProtocol if not found.
-		ProtocolType getProtocolByPortPair(PortPair port, bool exact = true) const
-		{
-			// Order of precedence:
-			// 1. Check for exact match of port pair
-			// 1.a If exact is true, return the protocol type if found, go to step 4 if not found
-			// 2. If not found, check for src port match
-			// 3. If not found, check for dst port match
-			// 4. If still not found, return UnknownProtocol
-
-			auto it = m_PortToProtocolMap.find(port);
-			if (it != m_PortToProtocolMap.end())
-			{
-				return it->second;
-			}
-			
-			if (exact)
-				return UnknownProtocol;  // Return UnknownProtocol if exact match not found
-
-			// Check for src port match
-			it = m_PortToProtocolMap.find(PortPair::fromSrc(port.portSrc));
-			if (it != m_PortToProtocolMap.end())
-			{
-				return it->second;
-			}
-
-			// Check for dst port match
-			it = m_PortToProtocolMap.find(PortPair::fromDst(port.portDst));
-			if (it != m_PortToProtocolMap.end())
-			{
-				return it->second;
-			}
-
-			return UnknownProtocol;  // Return UnknownProtocol if port not found
-		}
+		ProtocolType getProtocolByPortPair(PortPair port, bool exact = true) const;
 
 		/// @brief Check if a port matches a specific protocol type.
 		/// @param port The port number to check.
@@ -155,9 +112,9 @@ namespace pcpp
 		PortMapper portMapper;
 
 		/// @brief Creates a new instance of ParserConfiguration with default settings.
-		/// 
+		///
 		/// Prefer using `getDefault()` to obtain the default configuration if a new instance is not required.
-		/// 
+		///
 		/// @return A ParserConfiguration instance with default port mappings.
 		static ParserConfiguration makeDefaultConfiguration()
 		{
@@ -167,9 +124,9 @@ namespace pcpp
 		}
 
 		/// @brief Get the default parser configuration.
-		/// 
+		///
 		/// The returned reference can be used to configure the parser globally.
-		/// 
+		///
 		/// @return A reference to the default ParserConfiguration instance.
 		static inline ParserConfiguration& getDefault()
 		{
---
+++ Packet++/src/ParserConfig.cpp
@@ -0,0 +1,82 @@
+#include "ParserConfig.h"
+
+namespace pcpp
+{
+	void PortMapper::addPortMapping(PortPair port, ProtocolType protocol, bool symmetrical)
+	{
+		if (port == PortPair())
+		{
+			throw std::invalid_argument("PortPair cannot be empty (both src and dst ports are 0)");
+		}
+
+		auto insertResult = m_PortToProtocolMap.insert({ port, protocol });
+		insertResult.first->second = protocol;  // Update the protocol if it already exists
+		if (!insertResult.second)
+		{
+			PCPP_LOG_WARN("Port " << port << " is already mapped to protocol " << insertResult.first->second
+			                      << ", updating to " << protocol);
+		}
+
+		if (symmetrical && port.portSrc != port.portDst)
+		{
+			// Add the symmetrical mapping
+			PortPair symmetricalPort = { port.portDst, port.portSrc };
+			addPortMapping(symmetricalPort, protocol, false);
+		}
+	}
+
+	void PortMapper::removePortMapping(PortPair port, bool symmetrical)
+	{
+		auto it = m_PortToProtocolMap.find(port);
+		if (it != m_PortToProtocolMap.end())
+		{
+			m_PortToProtocolMap.erase(it);
+		}
+		else
+		{
+			PCPP_LOG_DEBUG("Port " << port << " not found in port mapper, nothing to remove");
+		}
+
+		if (symmetrical && port.portSrc != port.portDst)
+		{
+			// Remove the symmetrical mapping
+			PortPair symmetricalPort = { port.portDst, port.portSrc };
+			removePortMapping(symmetricalPort, false);
+		}
+	}
+
+	ProtocolType PortMapper::getProtocolByPortPair(PortPair port, bool exact) const
+	{
+		// Order of precedence:
+		// 1. Check for exact match of port pair
+		// 1.a If exact is true, return the protocol type if found, go to step 4 if not found
+		// 2. If not found, check for src port match
+		// 3. If not found, check for dst port match
+		// 4. If still not found, return UnknownProtocol
+
+		auto it = m_PortToProtocolMap.find(port);
+		if (it != m_PortToProtocolMap.end())
+		{
+			return it->second;
+		}
+
+		if (exact)
+			return UnknownProtocol;  // Return UnknownProtocol if exact match not found
+
+		// Check for src port match
+		it = m_PortToProtocolMap.find(PortPair::fromSrc(port.portSrc));
+		if (it != m_PortToProtocolMap.end())
+		{
+			return it->second;
+		}
+
+		// Check for dst port match
+		it = m_PortToProtocolMap.find(PortPair::fromDst(port.portDst));
+		if (it != m_PortToProtocolMap.end())
+		{
+			return it->second;
+		}
+
+		return UnknownProtocol;  // Return UnknownProtocol if port not found
+	}
+}
