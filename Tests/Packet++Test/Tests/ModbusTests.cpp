12b16500ee2462ae30c4359534e8d6876f722abb
yahyayozo <yahya.aouledamer@gmail.com>
2025-08-08 00:02:38 +0100

    adjust modbus creation test

--- Tests/Packet++Test/Tests/ModbusTests.cpp
+++ Tests/Packet++Test/Tests/ModbusTests.cpp
@@ -11,21 +11,16 @@ PTF_TEST_CASE(ModbusLayerCreationTest)
 	timeval time;
 	gettimeofday(&time, nullptr);
 
+	// Transaction ID: 0, Unit ID: 10, Function Code: 17
 	READ_FILE_AND_CREATE_PACKET(1, "PacketExamples/ModbusRequest.dat");
 
 	pcpp::Packet realPacket(&rawPacket1);
 	PTF_ASSERT_TRUE(realPacket.isPacketOfType(pcpp::Modbus));
-
 	pcpp::ModbusLayer* modbusLayerFromRealPacket = realPacket.getLayerOfType<pcpp::ModbusLayer>();
 
-	pcpp::ModbusLayer modbusLayer(12345, 1, 3);
+	pcpp::ModbusLayer modbusLayer(0, 10, 17);
 
-	PTF_ASSERT_EQUAL(modbusLayer.getTransactionId(), 12345);
-	PTF_ASSERT_EQUAL(modbusLayer.getProtocolId(), 0);
-	PTF_ASSERT_EQUAL(modbusLayer.getLength(), 2);  // minimum length of the MODBUS payload + unit_id
-	PTF_ASSERT_EQUAL(modbusLayer.getUnitId(), 1);
-	PTF_ASSERT_EQUAL(modbusLayer.getFunctionCode(), 3);
-	PTF_ASSERT_EQUAL(modbusLayer.getHeaderLen(), sizeof(pcpp::modbus_header));
+	PTF_ASSERT_BUF_COMPARE(modbusLayer.getData(), modbusLayerFromRealPacket->getData(), modbusLayer.getHeaderLen());
 
 	PTF_ASSERT_EQUAL(modbusLayer.getOsiModelLayer(), pcpp::OsiModelApplicationLayer);
 
@@ -53,12 +48,12 @@ PTF_TEST_CASE(ModbusLayerParsingTest)
 
 	pcpp::ModbusLayer* modbusLayer = packet.getLayerOfType<pcpp::ModbusLayer>();
 	PTF_ASSERT_NOT_NULL(modbusLayer);
-	PTF_ASSERT_EQUAL(modbusLayer->getTransactionId(), 1);
+	PTF_ASSERT_EQUAL(modbusLayer->getTransactionId(), 0);
 	PTF_ASSERT_EQUAL(modbusLayer->getProtocolId(), 0);
-	PTF_ASSERT_EQUAL(modbusLayer->getLength(), 6);
+	PTF_ASSERT_EQUAL(modbusLayer->getLength(), 2);
 	PTF_ASSERT_EQUAL(modbusLayer->getUnitId(), 10);
-	PTF_ASSERT_EQUAL(modbusLayer->getFunctionCode(), 1);
+	PTF_ASSERT_EQUAL(modbusLayer->getFunctionCode(), 17);
 
 	PTF_ASSERT_EQUAL(modbusLayer->toString(),
-	                 "Modbus Layer, Transaction ID: 1, Protocol ID: 0, Length: 6, Unit ID: 10, Function Code: 1");
+	                 "Modbus Layer, Transaction ID: 0, Protocol ID: 0, Length: 2, Unit ID: 10, Function Code: 17");
 }  // ModbusLayerParsingTest
