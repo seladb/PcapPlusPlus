c4fa3a7fb3957fd2ff3a7d5a22050f876bc2ee6e
Dimitar Krastev <dimi1010100@gmail.com>
2025-08-18 13:07:09 +0300

    Fix includes.

--- Tests/PcppTestUtilities/PacketFactory.hpp
+++ Tests/PcppTestUtilities/PacketFactory.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <string>
 #include <vector>
 
 #include "RawPacket.h"
--- Tests/PcppTestUtilities/Resources.hpp
+++ Tests/PcppTestUtilities/Resources.hpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <string>
 
+#include <cstdint>
+
 namespace pcpp_tests
 {
 	namespace utils
