e9cd5981375791a68c7f6e5ca474dc252284ff17
Dimitar Krastev <dimi1010100@gmail.com>
2025-08-24 13:22:53 +0300

    Added additional bounds checking for dataPos.

--- Packet++/src/TelnetLayer.cpp
+++ Packet++/src/TelnetLayer.cpp
@@ -131,6 +131,16 @@ namespace pcpp
 			return std::string();
 		}
 
+		// Possibly make this a DEBUG assertion so it is skipped in release builds
+		if (dataPos < m_Data || dataPos >= (m_Data + m_DataLen))
+		{
+			PCPP_LOG_ERROR("Data position is out of bounds, this should never happen! \n"
+			               " - DataPos: "
+			               << static_cast<void*>(dataPos) << ", Data Ptr: " << static_cast<void*>(m_Data)
+			               << ", DataLen: " << m_DataLen);
+			throw std::logic_error("Data position is out of bounds, this should never happen");
+		}
+
 		// Convert to string
 		if (removeEscapeCharacters)
 		{
