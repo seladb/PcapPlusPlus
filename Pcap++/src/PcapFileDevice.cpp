26e400ef491a9347c7fa7cce106584e1b47019f1
Dimitar Krastev <dimi1010100@gmail.com>
2025-09-26 21:27:44 +0300

    Lint

--- Pcap++/src/PcapFileDevice.cpp
+++ Pcap++/src/PcapFileDevice.cpp
@@ -276,8 +276,7 @@ namespace pcpp
 		{
 			throw std::runtime_error(
 			    "PcapPlusPlus was compiled without nano precision support which requires libpcap > 1.5.1. "
-			    "Please recompile PcapPlusPlus with nano precision support to use this feature."
-			);
+			    "Please recompile PcapPlusPlus with nano precision support to use this feature.");
 		}
 		m_Precision = FileTimestampPrecision::Microseconds;
 #endif
