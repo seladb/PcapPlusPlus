41fe18845a271c40f1f1347e23cba87d2f96e793
Dimitar Krastev <dimi1010100@gmail.com>
2025-10-03 11:31:06 +0300

    Fixed test output formatting.

--- Tests/Pcap++Test/TestDefinition.h
+++ Tests/Pcap++Test/TestDefinition.h
@@ -20,12 +20,12 @@ PTF_TEST_CASE(TestLogger);
 PTF_TEST_CASE(TestLoggerMultiThread);
 
 // Implemented in FileTests.cpp
-PTF_TEST_CASE(TestIFileReaderDeviceFactory_Pcap_MicroPrecision);
-PTF_TEST_CASE(TestIFileReaderDeviceFactory_Pcap_NanoPrecision);
-PTF_TEST_CASE(TestIFileReaderDeviceFactory_PcapNG);
-PTF_TEST_CASE(TestIFileReaderDeviceFactory_PcapNG_ZST);
-PTF_TEST_CASE(TestIFileReaderDeviceFactory_PcapNG_ZST_Unsupported);
-PTF_TEST_CASE(TestIFileReaderDeviceFactory_Invalid);
+PTF_TEST_CASE(TestFileDeviceFactory_Pcap_MicroPrecision);
+PTF_TEST_CASE(TestFileDeviceFactory_Pcap_NanoPrecision);
+PTF_TEST_CASE(TestFileDeviceFactory_PcapNG);
+PTF_TEST_CASE(TestFileDeviceFactory_PcapNG_ZST);
+PTF_TEST_CASE(TestFileDeviceFactory_PcapNG_ZST_Unsupported);
+PTF_TEST_CASE(TestFileDeviceFactory_Invalid);
 PTF_TEST_CASE(TestPcapFileReadWrite);
 PTF_TEST_CASE(TestPcapFileMicroPrecision);
 PTF_TEST_CASE(TestPcapFileNanoPrecision);
