9d089e3193b0257239f61d1fc252165c4fb8ccf5
Dimitar Krastev <dimi1010100@gmail.com>
2026-08-15 10:40:43 +0300

    Revert "Suppress lifetime warnings in PointerVector."
    This reverts commit 90b89491bde52523f56e998536a21defca77fe5a.

--- Common++/header/PointerVector.h
+++ Common++/header/PointerVector.h
@@ -177,14 +177,10 @@ namespace pcpp
 
 			// Release is called after the raw pointer is already inserted into the vector to prevent
 			// a memory leak if push_back throws.
-
-			// cppcheck-suppress-begin [danglingLifetime, ignoredReturnValue]
+			// cppcheck-suppress danglingLifetime
 			m_Vector.push_back(element.get());
 			element.release();
-			// cppcheck-suppress-end [danglingLifetime, ignoredReturnValue]
 		}
-
-		// cppcheck-suppress-begin danglingLifetime
 
 		/// Get the first element of the vector
 		/// @return An iterator object pointing to the first element of the vector
@@ -213,8 +209,6 @@ namespace pcpp
 		{
 			return m_Vector.end();
 		}
-
-		// cppcheck-suppress-end danglingLifetime
 
 		/// Get number of elements in the vector
 		/// @return The number of elements in the vector
