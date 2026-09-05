90f606af231a45f8a61f37f275b1ae63229bb248
Ege Çetin <64282645+egecetin@users.noreply.github.com>
2026-09-05 13:49:39 +0300

    Fix Heap-buffer-overflow READ 4 · pcpp::GREv1Layer::getAcknowledgmentNum (#2243)
    * Fix Heap-buffer-overflow READ 4 · pcpp::GREv1Layer::getAcknowledgmentNum
    
    * simple subtract is ok
    
    * fix warning
    
    * it should return value if explicitly requested
    
    * might be equal
    
    * format

--- Packet++/src/GreLayer.cpp
+++ Packet++/src/GreLayer.cpp
@@ -85,7 +85,9 @@ namespace pcpp
 
 			if (field == curField)
 			{
-				if (curFieldExists || returnOffsetEvenIfFieldMissing)
+				// If the packet is truncated/malformed it might be overflow packet length
+				if ((curFieldExists && static_cast<size_t>(std::abs(ptr - m_Data)) <= m_DataLen) ||
+				    returnOffsetEvenIfFieldMissing)
 					return origPtr;
 
 				return nullptr;
